df9c59e4ead8fa214bf7a6d46509eb846c68977e
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-11-14 23:07:12 +0200

    Add EdgeGesture service
    Based on code of PieService
    
    Change-Id: I053737e36340a5ec6814b21eb87575f9689c7e88

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -83,6 +83,7 @@ import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
 import com.android.server.fingerprint.FingerprintService;
 import com.android.server.hdmi.HdmiControlService;
+import com.android.server.gesture.EdgeGestureService;
 import com.android.server.gesture.GestureService;
 import com.android.server.input.InputManagerService;
 import com.android.server.job.JobSchedulerService;
@@ -999,6 +1000,7 @@ public final class SystemServer {
         ILockSettings lockSettings = null;
         MediaRouterService mediaRouter = null;
         GestureService gestureService = null;
+        EdgeGestureService edgeGestureService = null;
 
         // Bring up services needed for UI.
         if (mFactoryTestMode != FactoryTest.FACTORY_TEST_LOW_LEVEL) {
@@ -1634,6 +1636,13 @@ public final class SystemServer {
             mSystemServiceManager.startService(ScreenStabilization.class);
             traceEnd();
 
+            try {
+                Slog.i(TAG, "EdgeGesture service");
+                edgeGestureService = new EdgeGestureService(context, inputManager);
+                ServiceManager.addService("edgegestureservice", edgeGestureService);
+            } catch (Throwable e) {
+                Slog.e(TAG, "Failure starting EdgeGesture service", e);
+            }
         }
 
         if (!isWatch) {
@@ -1815,8 +1824,14 @@ public final class SystemServer {
             reportWtf("making Display Manager Service ready", e);
         }
         traceEnd();
-
         mSystemServiceManager.setSafeMode(safeMode);
+        if (edgeGestureService != null) {
+            try {
+                edgeGestureService.systemReady();
+            } catch (Throwable e) {
+                reportWtf("making EdgeGesture service ready", e);
+            }
+        }
 
         // Start device specific services
         traceBeginAndSlog("StartDeviceSpecificServices");
