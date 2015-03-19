60813b17683f5a35479363b5943bac8e79b4bb4a
Roman Birg <roman@cyngn.com>
2015-03-19 14:32:33 -0700

    SystemUI: small optimization for notification icon numbers
    Don't create the paint object used to draw notification icon count
    badges in the status bar unless the user actually wants to see those
    counts.
    
    Change-Id: Iabba316102583a798acdc124d9fb51c0d7826a0d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -155,10 +155,6 @@ public class StatusBarIconView extends AnimatedImageView {
         mDozer = new NotificationIconDozeHelper(context);
         mBlocked = blocked;
         mSlot = slot;
-        mNumberPain = new Paint();
-        mNumberPain.setTextAlign(Paint.Align.CENTER);
-        mNumberPain.setColor(context.getColor(R.drawable.notification_number_text_color));
-        mNumberPain.setAntiAlias(true);
         setNotification(sbn);
         maybeUpdateIconScaleDimens();
         setScaleType(ScaleType.CENTER);
@@ -292,6 +288,10 @@ public class StatusBarIconView extends AnimatedImageView {
             if (icon.number > 0 && getContext().getResources().getBoolean(
                         R.bool.config_statusBarShowNumber)) {
                 if (mNumberBackground == null) {
+                    mNumberPain = new Paint();
+                    mNumberPain.setTextAlign(Paint.Align.CENTER);
+                    mNumberPain.setColor(getContext().getColor(R.drawable.notification_number_text_color));
+                    mNumberPain.setAntiAlias(true);
                     mNumberBackground = getContext().getResources().getDrawable(
                             R.drawable.ic_notification_overlay);
                 }
@@ -299,6 +299,7 @@ public class StatusBarIconView extends AnimatedImageView {
             } else {
                 mNumberBackground = null;
                 mNumberText = null;
+                mNumberPain = null;
             }
             invalidate();
         }
