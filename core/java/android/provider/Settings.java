562e16bdb5df924fc53b2d610334f05f37368fc9
Shuhao Wu <shuhao@shuhaowu.com>
2015-06-22 20:21:14 -0400

    Allow toggling animations off [1/2]
    @neobuddyy89: Rewrite code with below changes
    * Adapt for Android O
    * Move settings to Global with other animation settings
    * Fixed disabling logic

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12766,6 +12766,12 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * Disabled Transition animations of the android system
+         * @hide
+         */
+        public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
