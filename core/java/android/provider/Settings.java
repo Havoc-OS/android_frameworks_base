38cc1cfca200fec5fdaec1896e39fbfc1eb4126d
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5760,6 +5760,13 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
