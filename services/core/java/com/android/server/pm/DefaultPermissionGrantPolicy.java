d6f04708376d875d81f048d3433bde8193400ad1
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6
    
    OmniJaws: Fix More Settings Intent
    
    Change-Id: I982f09d6ba5beadb5c38eb52029eff1b95cec04c
    
    base: qs weather: be nice to people without OmniJaws
    
    if OmniJaws is not available or disabled show an informative
    Toast. Also change the default tile image to show also some sun :)
    
    Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd
    
    base: SystemUI: remove force weather update bits
    
    force updates have been removed from service API
    
    Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b
    
    base: SystemUI: use new OmniJaws error broadcasts
    
    Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65
    
    base: SystemUI: dont hide weather forcast data on errors
    
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0
    
    base: SystemUI: dont show OmniJaws error that prominent
    
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced
    
    base: SystemUI: add dedicated TextView for OmniJaws error marker
    
    dont mess with the layout in the error case
    and fix proper layout handling for loooong city names
    
    Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -888,6 +888,13 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(conpro2Package, STORAGE_PERMISSIONS, true, userId);
             }
 
+            // OmniJaws
+            PackageParser.Package omnijawspackage = getSystemPackageLPr(
+                    "org.omnirom.omnijaws");
+            if (omnijawspackage != null && doesPackageSupportRuntimePermissions(omnijawspackage)) {
+                grantRuntimePermissionsLPw(omnijawspackage, LOCATION_PERMISSIONS, userId);
+            }
+
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
         }
     }
