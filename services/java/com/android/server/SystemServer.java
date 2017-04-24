a51e9eb415f20d948d5459a22d0b657e686056d1
Chris Lahaye <dev@chrislahaye.com>
2017-04-25 00:10:35 +0200

    pocket: introduce pocket bridge
    This service communicates pocket state to the pocket judge kernel driver.
    It maintains the pocket state by binding to the pocket service.
    
    Ticket: NOUGAT-49

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -92,6 +92,7 @@ import com.android.server.om.OverlayManagerService;
 import com.android.server.os.DeviceIdentifiersPolicyService;
 import com.android.server.os.SchedulingPolicyService;
 import com.android.server.pocket.PocketService;
+import com.android.server.pocket.PocketBridgeService;
 import com.android.server.pm.BackgroundDexOptService;
 import com.android.server.pm.Installer;
 import com.android.server.pm.LauncherAppsService;
@@ -1557,6 +1558,13 @@ public final class SystemServer {
             Slog.i(TAG, "Starting ScreenStabilization Service");
             mSystemServiceManager.startService(ScreenStabilization.class);
         }
+
+            if (!context.getResources().getString(
+                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
+                traceBeginAndSlog("StartPocketBridgeService");
+                mSystemServiceManager.startService(PocketBridgeService.class);
+                traceEnd();
+            }
 
         if (!disableNonCoreServices && !disableMediaProjection) {
             traceBeginAndSlog("StartMediaProjectionManager");
