ed5baf4aa1531d4423cdd030038d90973cdacf5b
Corinna Vinschen <xda@vinschen.de>
2017-08-12 13:42:58 +0200

    PowerManager: Allow to distinguish different keypresses
    * Use keypress info to exclude pressing volume keys from
      illuminating HW buttons in config_buttonLightOnKeypressOnly
      mode.
    
    Change-Id: I6bfc7ddd075e12e1ad10c3663a63e80c8d7f983d
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1413,7 +1413,8 @@ public final class PowerManagerService extends SystemService
             } else {
                 if (eventTime > mLastUserActivityTime) {
                     mButtonPressed = event == PowerManager.USER_ACTIVITY_EVENT_BUTTON;
-                    if ((mButtonBacklightOnTouchOnly && mButtonPressed)
+                    if ((mButtonBacklightOnTouchOnly && mButtonPressed &&
+                        (flags & PowerManager.USER_ACTIVITY_FLAG_NO_BUTTON_LIGHTS) == 0)
                             || eventTime == mLastWakeTime) {
                         mButtonPressed = true;
                         mLastButtonActivityTime = eventTime;
