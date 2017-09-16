154577357b8f407ff85e688ca3108d27c9b43d87
ezio84 <brabus84@gmail.com>
2017-09-16 21:11:37 +0200

    Allow to customize notification led light [2/2]

--- core/java/android/app/NotificationChannel.java
+++ core/java/android/app/NotificationChannel.java
@@ -436,7 +436,6 @@ public final class NotificationChannel implements Parcelable {
      *
      * Only modifiable before the channel is submitted to
      * {@link NotificationManager#notify(String, int, Notification)}.
-     * @hide
      */
     public void setLightOnTime(int time) {
         this.mLightOnTime = time;
@@ -448,7 +447,6 @@ public final class NotificationChannel implements Parcelable {
      *
      * Only modifiable before the channel is submitted to
      * {@link NotificationManager#notify(String, int, Notification)}.
-     * @hide
      */
     public void setLightOffTime(int time) {
         this.mLightOffTime = time;
@@ -602,7 +600,6 @@ public final class NotificationChannel implements Parcelable {
     /**
      * Returns the notification light ON time for notifications posted to this channel. Irrelevant
      * unless {@link #shouldShowLights()}.
-     * @hide
      */
     public int getLightOnTime() {
         return mLightOnTime;
@@ -611,7 +608,6 @@ public final class NotificationChannel implements Parcelable {
     /**
      * Returns the notification light OFF time for notifications posted to this channel. Irrelevant
      * unless {@link #shouldShowLights()}.
-     * @hide
      */
     public int getLightOffTime() {
         return mLightOffTime;
