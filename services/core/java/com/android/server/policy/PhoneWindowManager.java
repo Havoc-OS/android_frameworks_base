26f39da1ad72e302a4949e91f52b6c98077c9d45
Arne Coucheron <arco68@gmail.com>
2017-09-27 02:25:32 +0200

    fw/base: Enable home button wake
    [jprimero15: don't disable home button key when in ambient and when screen is off so that we can use it as a wake key]
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4473,7 +4473,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         // Disable hw keys in Ambient and when screen off
-        if ((isDozeMode() || !isScreenOn()) && (appSwitchKey || homeKey || menuKey || backKey)) {
+        if ((isDozeMode() || !isScreenOn()) && (appSwitchKey || menuKey || backKey)) {
             return 0;
         }
 
@@ -4796,6 +4796,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 break;
             }
+
+            case KeyEvent.KEYCODE_HOME:
+                if (down && !interactive) {
+                    isWakeKey = true;
+                }
+                break;
 
             case KeyEvent.KEYCODE_ENDCALL: {
                 result &= ~ACTION_PASS_TO_USER;
