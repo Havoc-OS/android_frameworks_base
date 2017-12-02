076f99f477e53505b47c5495c18d89407ba98521
ezio84 <brabus84@gmail.com>
2017-12-02 15:58:34 +0100

    QS tiles customizer: respect user tiles columns value
    Change-Id: I88c5d5961af68b265ef6cef601d0cc97cd9a4c85

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -352,6 +352,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         if (mTileLayout != null) {
             mTileLayout.updateResources();
         }
+        if (mCustomizePanel != null) {
+            mCustomizePanel.updateResources();
+        }
     }
 
     @Override
