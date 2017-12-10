fd2db0558003e98038f7efc94de3eaac917de34f
maxwen <max.weninger@gmail.com>
2017-12-10 09:30:50 +0100

    base: Use new gradient dialog also for recovery/bootloader action
    abc: adapted to our code
    
    original commit: https://gerrit.omnirom.org/#/c/27327/
    
    Change-Id: I3a05ca554927dacff7e10d901de5947569a08d07

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -325,6 +325,9 @@ public final class ShutdownThread extends Thread {
                 pd.setIndeterminate(true);
             }
         } else if (mReason != null && mReason.equals(PowerManager.REBOOT_BOOTLOADER)) {
+            if (showSysuiReboot()) {
+                return null;
+            }
             pd.setTitle(context.getText(com.android.internal.R.string.reboot_to_bootloader_title));
             pd.setMessage(context.getText(
                         com.android.internal.R.string.reboot_to_bootloader_message));
