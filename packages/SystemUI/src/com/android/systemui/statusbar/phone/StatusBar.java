f250a51c33180e42063151328d410ef322c3aa79
ezio84 <brabus84@gmail.com>
2018-04-04 14:37:40 +0200

    Slim Recents: albumart as player card icon when playing a song [2/2]
    Change-Id: Ib5356d9413a1cb8aab59cea85a1d234548cd9413
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2235,7 +2235,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mNavigationBar.setPulseColors(n.isColorizedMedia(), colors);
             }
             if (mSlimRecents != null) {
-                mSlimRecents.setMediaColors(n.isColorizedMedia(), colors);
+                mSlimRecents.setMediaColors(n.isColorizedMedia(), colors,
+                        n.getOriginalLargeIcon().loadDrawable(mContext));
             }
         }
     }
