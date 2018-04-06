e409be99a586fe893f191f33c02dba875e6881f7
Ali B <abittin@gmail.com>
2018-04-06 16:28:25 +0300

    SystemUI: QS footer visibilities [1/2]
    Enable toggling visibilities of running services and
    settings icons in the quicksettings footer.
    
    For settings icon, we are actually hiding entire
    settingscontainer when toggled on. So even if tuner
    is enabled, that will also be hidden.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5426,6 +5426,20 @@ public final class Settings {
         public static final String RECENTS_OMNI_SWITCH_ENABLED = "recents_omni_switch";
 
         /**
+         * Show Settings icon in QS Footer
+         *
+         * @hide
+         */
+        public static final String QSFOOTER_SHOW_SETTINGS = "qs_footer_show_settings";
+
+        /**
+         *
+         * Show Running Services icon in QS Footer.
+         * @hide
+         */
+        public static final String QSFOOTER_SHOW_SERVICES = "qs_footer_show_services";
+
+        /**
          * change lockscreen security alpha color
          * @hide
          */
