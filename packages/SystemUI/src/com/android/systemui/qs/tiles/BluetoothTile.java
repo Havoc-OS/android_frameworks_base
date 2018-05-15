8b325613a9492ac907bcaa8ef6832fbc490e3c67
Song Gao <songg@codeaurora.org>
2018-05-15 14:10:29 +0800

    SystemUI: Add null pointer check in BluetoothTile
    Before forwarding to the main process of handleSetListening()
    and handleUpdateState(), check null pointer to avoid
    NullPointerException.
    
    Change-Id: I03f1e98fd92f2afb64522d370c0e3a9486fe518e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -240,6 +240,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
 
     @Override
     public boolean isAvailable() {
+        if (mController == null) return false;
         return mController.isBluetoothSupported();
     }
 
