2dbf6e242a9aecdc8b46a56a6c3c73d34d1b8a0b
Joey <joey@lineageos.org>
2018-05-16 17:16:06 +0200

    SystemUI: Add reading mode tile
    Author: Joey <joey@lineageos.org>
    Date:   Wed May 16 17:16:06 2018 +0200
    
        base: add reading mode tile
    
        Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sat May 26 00:58:57 2018 +0300
    
        SystemUI: Fix reading mode tile accessibility strings
    
        Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Wed Aug 8 18:59:25 2018 +0200
    
        SystemUI: Update reading mode tile for new API
    
        Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d
    
    Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -79,6 +80,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<UiModeNightTile> mUiModeNightTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<LiveDisplayTile> mLiveDisplayTileProvider;
+    private final Provider<ReadingModeTile> mReadingModeTileProvider;
 
     private QSTileHost mHost;
 
@@ -103,7 +105,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<GarbageMonitor.MemoryTile> memoryTileProvider,
             Provider<UiModeNightTile> uiModeNightTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
-            Provider<LiveDisplayTile> liveDisplayTileProvider) {
+            Provider<LiveDisplayTile> liveDisplayTileProvider,
+            Provider<ReadingModeTile> readingModeTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -125,6 +128,7 @@ public class QSFactoryImpl implements QSFactory {
         mUiModeNightTileProvider = uiModeNightTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mLiveDisplayTileProvider = liveDisplayTileProvider;
+        mReadingModeTileProvider = readingModeTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -182,6 +186,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCaffeineTileProvider.get();
             case "livedisplay":
                 return mLiveDisplayTileProvider.get();
+            case "reading_mode":
+                return mReadingModeTileProvider.get();
         }
 
         // Intent tiles.
