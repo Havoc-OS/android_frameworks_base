85dc4eddee3505cbcad8fcecfc12a988623c42d0
Lars Greiss <kufikugel@googlemail.com>
2018-05-23 00:03:40 +0530

    Fw_base - Keyguard - Add user configurable shortcuts [1/2]
    Frameworks: Add several Slim helper classes and constants back
    
    Frameworks: smart pulldown we do not support anymore
    
    No QS nothing smart needed anymore
    
    Frameworks: inital bringup of slim actions and helper classes
    
    Bring back slimactions.
    
    - Most actions are available and adapted to LP (mainly statusbar actions
    we did on kk need to be ported back again)
    - Cleanup of old code
    - Renamed classes and methods
    
    and more.....
    
    This is the base for all the comming features now
    
    Frameworks: add all slim shortcut image resources
    
    Importing for now the old one from KitKat. Graphics group is currently
    vectorizing and revamping all. When they are finished we overwrite them
    and
    add xxxhdpi ones as well.
    
    Doing this now due that it is stopping the progress on a bunch of
    features right now
    
    And a small method renaming to be inline with the other methods
    
    Frameworks: add shortcut action icons
    
    Frameworks: Keyguard - Add user configurable shortcuts
    
    Change-Id: I32f86a067b6d54af32a1845cb1741ee55142d5d4
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4696,6 +4696,21 @@ public final class Settings {
         public static final String LOCK_SCREEN_SHOW_CARRIER = "lock_screen_show_carrier";
 
         /**
+         * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+        /**
+         * Whether shorcuts open with normal or longpress
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS_LONGPRESS =
+                "lockscreen_shortcuts_longpress";
+
+        /**
          * Quick settings carrier label
          * 0: Hide
          * 1: Display on status bar
