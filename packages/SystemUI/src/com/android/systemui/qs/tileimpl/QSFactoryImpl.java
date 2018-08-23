e58653610ef6fc9a5e50d37827e33533ba1c9d1e
ZeNiXxX <vitjahermann@gmail.com>
2018-08-23 08:50:55 +0200

    SystemUI: Add Calculator tile
    Added to pie by @ZeNiXxX

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,6 +49,7 @@ import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.CalcTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -135,6 +136,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CameraTile(mHost);
             case "alarm":
                 return new AlarmTile(mHost);
+            case "calc":
+                return new CalcTile(mHost);
         }
 
         // Intent tiles.
