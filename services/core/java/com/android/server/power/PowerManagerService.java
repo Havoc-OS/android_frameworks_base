60e69a8178b5bf577e9d332e04cc34d77c7f5abb
Danesh M <daneshm90@gmail.com>
2018-09-14 05:21:21 -0500

    Ensure proximity check is taken into consideration
    Only do the proximity check if explicitly being asked to do so.
    This is a patch that was never ported from cm-11.0 :
    2293e60c7de011782e59486605c04b9f67a1e24f
    
    Change-Id: I8fea883d8fb881df0853754ac973a453f63b59d1

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -4585,7 +4585,11 @@ public final class PowerManagerService extends SystemService
                     }
                 }
             };
-            runWithProximityCheck(r);
+            if (checkProximity) {
+                runWithProximityCheck(r);
+            } else {
+                r.run();
+            }
         }
 
         @Override // Binder call
