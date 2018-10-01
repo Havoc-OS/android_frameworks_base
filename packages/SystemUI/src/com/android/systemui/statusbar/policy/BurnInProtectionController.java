7a5dc330abe6b61a0cd7c68661d64543a2cd44ee
mydongistiny <jaysonedson@gmail.com>
2018-10-01 06:24:04 -0700

    BurnInProtection: Fix null object reference with timer
    Happens when after flashing a build you let the screen turn off before unlocking
    
    Change-Id: I051b75531bf65c8cf3bbc4779fafdbae1a156686
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BurnInProtectionController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BurnInProtectionController.java
@@ -104,6 +104,7 @@ public class BurnInProtectionController {
 
     public void stopSwiftTimer() {
         if (!mSwiftEnabled) return;
+        if (mTimer == null) return;
         mTimer.cancel();
         mTimer.purge();
         mTimer = null;
