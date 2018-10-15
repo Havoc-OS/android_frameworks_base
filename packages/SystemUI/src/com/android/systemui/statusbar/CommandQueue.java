68176bb31ce38b2372af8d96a59ba34c2014d257
Paul Henschel <drcmda@gmail.com>
2018-10-15 14:53:09 +0200

    [SQUASH] PIE 3.0 (oreo->Pie)[1/3]
    Pie is an alternative navigation and system info interface. It replaces the
    standard navbar/statusbar with a navigation arc decorated with statusbar info.
    
    This also contains full theming support.
    
    Bring it toghter !
    
    Bring it to Pie @ZeNiXxX
    
    FWB:Fix Pie positions & remove restrictions on immersive mode
    
    PIE:Fixes for O
    
    --Adapt to the new APIs
    --Compilation fixes
    
    Bring to Pie by @ZeNiXxX
    @SKULSHADY Code cleanup and tile improvements

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -96,6 +96,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 47 << MSG_SHIFT;
     private static final int MSG_SET_AUTOROTATE_STATUS         = 48 << MSG_SHIFT;
     private static final int MSG_RESTART_UI                    = 49 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_PIE_ORIENTATION = 43 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -362,6 +363,13 @@ public class CommandQueue extends IStatusBar.Stub {
         synchronized (mLock) {
             mHandler.removeMessages(MSG_SHOW_PICTURE_IN_PICTURE_MENU);
             mHandler.obtainMessage(MSG_SHOW_PICTURE_IN_PICTURE_MENU).sendToTarget();
+        }
+    }
+
+    public void toggleOrientationListener(boolean enable) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_PIE_ORIENTATION);
+            mHandler.obtainMessage(MSG_TOGGLE_PIE_ORIENTATION).sendToTarget();
         }
     }
 
@@ -839,6 +847,11 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_RESTART_UI:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).restartUI();
+                    }
+                    break;
+                case MSG_TOGGLE_PIE_ORIENTATION:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleOrientationListener(msg.arg1 != 0);
                     }
                     break;
             }
