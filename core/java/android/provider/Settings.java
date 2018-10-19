4584538907f5ac5bdfc23b6262c4a8e6834b7c4d
SKULSHADY <anushekprasal@gmail.com>
2018-10-20 02:45:22 +0530

    Material toast icon switch [1/2]
    Also fix the toast layout to center the message and enable it by default.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5742,6 +5742,12 @@ public final class Settings {
         private static final Validator ONE_HAND_MODE_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Toast icon
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5968,6 +5974,7 @@ public final class Settings {
 	        PRIVATE_SETTINGS.add(HIDE_LOCKSCREEN_ICON);
             PRIVATE_SETTINGS.add(QS_TILE_STYLE);       
             PRIVATE_SETTINGS.add(ONE_HAND_MODE_ENABLED);
+            PRIVATE_SETTINGS.add(TOAST_ICON);
         }
 
         /**
