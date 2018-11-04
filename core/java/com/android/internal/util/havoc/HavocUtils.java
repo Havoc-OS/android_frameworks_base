90a98e76be6b44276e55fdace7435a558986d138
ezio84 <brabus84@gmail.com>
2018-11-05 00:08:01 +0100

    Full gestures nav: longpress to move keyboard cursors [1/2]
    also add a touch slop check for doubletap

--- core/java/com/android/internal/util/havoc/HavocUtils.java
+++ core/java/com/android/internal/util/havoc/HavocUtils.java
@@ -185,10 +185,7 @@ public class HavocUtils {
             }
         }
     }
-	
-    /**
-     * @hide
-     */
+
     public static void sendKeycode(int keycode) {
         long when = SystemClock.uptimeMillis();
         final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
@@ -196,7 +193,8 @@ public class HavocUtils {
                 KeyEvent.FLAG_FROM_SYSTEM,
                 InputDevice.SOURCE_KEYBOARD);
         final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
-         final Handler handler = new Handler(Looper.getMainLooper());
+
+        final Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
             public void run() {
@@ -212,6 +210,29 @@ public class HavocUtils {
             }
         }, 20);
     }
+
+    public static void sendKeycode(int keycode, Handler h) {
+        long when = SystemClock.uptimeMillis();
+        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
+                0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
+                KeyEvent.FLAG_FROM_SYSTEM,
+                InputDevice.SOURCE_KEYBOARD);
+        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evDown,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        });
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evUp,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        }, 20);
+    }
 	
     public static void takeScreenshot(boolean full) {
         IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
@@ -220,6 +241,18 @@ public class HavocUtils {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void moveKbCursor(int action, boolean right) {
+        int code = right ? KeyEvent.KEYCODE_DPAD_RIGHT : KeyEvent.KEYCODE_DPAD_LEFT;
+        long downTime = System.currentTimeMillis();
+        long when = downTime;
+        final KeyEvent ev = new KeyEvent(downTime, when, action, code, 0,
+                0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
+                (KeyEvent.FLAG_SOFT_KEYBOARD | KeyEvent.FLAG_KEEP_TOUCH_MODE),
+                InputDevice.SOURCE_KEYBOARD);
+        InputManager.getInstance().injectInputEvent(ev,
+                InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
     }
 
     public static ActivityInfo getRunningActivityInfo(Context context) {
