3743bf6d3b13e4cf4098759e1f957433d8926da5
out386 <ritayanout@gmail.com>
2018-11-05 18:50:00 -0700

    Add Pulse Lava Lamp start and end colors (1/3)
    Change-Id: I23ba4a254266724a4519e577abe1039985968f4b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5451,6 +5451,20 @@ public final class Settings {
         public static final String PULSE_APPS_BLACKLIST = "pulse_apps_blacklist";
 
         /**
+         * Fling pulse lavalamp start and end colors
+         *
+         * @hide
+         */
+        public static final String FLING_PULSE_LAVALAMP_COLOR_FROM = "fling_lavalamp_color_from";
+
+        /**
+         * Fling pulse lavalamp start and end colors
+         *
+         * @hide
+         */
+        public static final String FLING_PULSE_LAVALAMP_COLOR_TO = "fling_lavalamp_color_to";
+
+        /**
          *  Weather to use a custom color for the visualizer on the lock screen
          * @hide
          */
