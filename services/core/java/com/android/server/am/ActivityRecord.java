63076fbade3f749574a84a2fdb7f7824bf08bbab
Vijay Dandiga <vdandiga@codeaurora.org>
2018-11-06 18:31:28 +0530

    uxe: Check memory pressure before starting empty app.
    Check memory pressure before starting empty app.
    
    CRs-Fixed: 2351001
    Change-Id: Ibe9a97c745fb952c125136ec9b5dab3279965858
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -185,6 +185,7 @@ import android.view.RemoteAnimationDefinition;
 import android.view.WindowManager.LayoutParams;
 
 import com.android.internal.R;
+import com.android.internal.app.procstats.ProcessStats;
 import com.android.internal.app.ResolverActivity;
 import com.android.internal.content.ReferrerIntent;
 import com.android.internal.util.XmlUtils;
@@ -814,7 +815,8 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
         @Override
         protected Void doInBackground(Void... params) {
             String res = null;
-            if (mUxPerf != null) {
+            if (mUxPerf != null
+                    && service.getMemoryTrimLevel() < ProcessStats.ADJ_MEM_FACTOR_CRITICAL) {
                 res = mUxPerf.perfUXEngine_trigger(BoostFramework.UXE_TRIGGER);
                 if (res == null)
                     return null;
