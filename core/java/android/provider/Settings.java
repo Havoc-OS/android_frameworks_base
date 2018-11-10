5edca616fb60fc330b476f246da34152b53f71d6
ezio84 <brabus84@gmail.com>
2018-11-10 20:43:31 +0100

    Stock Recents/Overview icon pack support [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6223,6 +6223,12 @@ public final class Settings {
         private static final Validator QS_PANEL_BG_ALPHA_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether to use icon pack for Recents
+         * @hide
+         */
+        public static final String RECENTS_ICON_PACK = "recents_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
