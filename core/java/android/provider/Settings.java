13caeef2b5fe4233faab14d17f808db551653676
varund7726 <date.varun123@gmail.com>
2018-11-26 14:02:45 -0700

    Smarbar Pixel Navbar Animation Duration Settings [1/3]
    Change-Id: Ib0b86cc25f22157f43b3612bbf43e07a61ac36ef

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10358,7 +10358,6 @@ public final class Settings {
          */
         public static final String PULSE_FADING_BLOCKS_OPACITY = "pulse_fading_blocks_opacity";
 
-
         /**
          * Whether to enable double tap to sleep for smartbar
          * @hide
@@ -10384,10 +10383,52 @@ public final class Settings {
         public static final String SMARTBAR_BUTTON_ANIMATION_STYLE = "smartbar_button_animation_style";
 
         /**
-         * Use black arrow theme instead of the white version.
+         * Pixel Anim Duration
          * @hide
          */
-        public static final String EDGE_GESTURES_BACK_USE_BLACK_ARROW = "edge_gestures_back_use_black_arrow";
+        public static final String OPA_ANIM_DURATION_Y = "opa_anim_duration_y";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String OPA_ANIM_DURATION_X = "opa_anim_duration_x";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String COLLAPSE_ANIMATION_DURATION_RY = "collapse_anim_duration_ry";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String COLLAPSE_ANIMATION_DURATION_BG = "collapse_anim_duration_bg";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String RETRACT_ANIMATION_DURATION = "retract_anim_duration";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String DIAMOND_ANIMATION_DURATION = "diamond_anim_duration";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String DOTS_RESIZE_DURATION = "dots_anim_duration";
+
+        /**
+         * Pixel Anim Duration
+         * @hide
+         */
+        public static final String HOME_RESIZE_DURATION = "home_resize_anim_duration";
 
         /**
          * This are the settings to be backed up.
