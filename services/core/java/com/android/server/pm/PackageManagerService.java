ccae795caffc5cabbdafd1712704c70daf83e763
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-11-27 02:34:24 -0600

    Ignore vendor overlays that must be on system/app
    Change-Id: Ie50c14fed0e573dca0d98d5301240a691375a99e

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -580,6 +580,8 @@ public class PackageManagerService extends IPackageManager.Stub
 
     private static final String[] PACKAGE_OVERLAY_DIRS = { "/system/overlay", "/vendor/overlay" };
 
+    private static final String VENDOR_OVERLAY_DIR = "/vendor/overlay";
+
     private static final String PRODUCT_OVERLAY_DIR = "/product/overlay";
 
     private static final String THEME_OVERLAY_DIR = "/data/system/theme";
@@ -8535,6 +8537,12 @@ public class PackageManagerService extends IPackageManager.Stub
         }
     }
 
+    private String[] systemOverlayPackages = {"SysuiDarkTheme",
+                                              "DisplayCutoutEmulationCorner",
+                                              "DisplayCutoutEmulationDouble",
+                                              "DisplayCutoutEmulationNarrow",
+                                              "DisplayCutoutEmulationWide"};
+
     private void scanDirLI(File scanDir, int parseFlags, int scanFlags, long currentTime) {
         final File[] files = scanDir.listFiles();
         if (ArrayUtils.isEmpty(files)) {
@@ -8556,6 +8564,11 @@ public class PackageManagerService extends IPackageManager.Stub
                         && !PackageInstallerService.isStageName(file.getName());
                 if (!isPackage) {
                     // Ignore entries which are not packages
+                    continue;
+                }
+                // Ignore vendor overlays that should live on system/app
+                if ((scanDir.getPath() == VENDOR_OVERLAY_DIR || scanDir.getPath() == PRODUCT_OVERLAY_DIR)
+                        && Arrays.asList(systemOverlayPackages).contains(file.getName())){
                     continue;
                 }
                 parallelPackageParser.submit(file, parseFlags);
