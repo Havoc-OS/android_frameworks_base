f61f4441fb6d018b93849f1e145c3e3f42c42e11
katao <katao@xiaomi.com>
2018-12-07 09:44:20 +0800

    Change the listening logic of MtpStorageManager
    Need to adjust the current monitoring logic, in order to accurately
    transfer file propertys to Windows. Current logic is that system
    called handleAddedObject() immediately when the large file is created,
    and transfer the defect state of the file, which will cause the Windows
    system to get an incomplete data error when getting the file.
    
    Test: boot phone,connect win10, enable mtp, push a file(>2M,such as ZIP)
    Get this file from your phone to your computer，check the file.
    Signed-off-by: katao <katao@xiaomi.com>
    
    Change-Id: I6f5db78652d63ffb88535d49f5567f3c7f06c596
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/java/android/mtp/MtpStorageManager.java
+++ media/java/android/mtp/MtpStorageManager.java
@@ -57,7 +57,7 @@ public class MtpStorageManager {
 
         MtpObjectObserver(MtpObject object) {
             super(object.getPath().toString(),
-                    MOVED_FROM | MOVED_TO | DELETE | CREATE | IN_ONLYDIR);
+                    MOVED_FROM | MOVED_TO | DELETE | CREATE | IN_ONLYDIR | CLOSE_WRITE);
             mObject = object;
         }
 
@@ -69,7 +69,9 @@ public class MtpStorageManager {
                     Log.e(TAG, "Received Inotify overflow event!");
                 }
                 MtpObject obj = mObject.getChild(path);
-                if ((event & MOVED_TO) != 0 || (event & CREATE) != 0) {
+                if (((event & IN_ISDIR) != 0 && (event & CREATE) != 0) ||
+                    ((event & IN_ISDIR) == 0 && (event & CLOSE_WRITE) != 0) ||
+                    ((event & MOVED_TO) != 0)) {
                     if (sDebug)
                         Log.i(TAG, "Got inotify added event for " + path + " " + event);
                     handleAddedObject(mObject, path, (event & IN_ISDIR) != 0);
