2083845612fad6c6158bdd5646077891c46a7d03
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 11:53:23 +0700

    Switching audio panel position [1/2]
    Change-Id: I6ebb5c6201a8f1e8e90dde005c23e505d874db8f
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4628,6 +4628,9 @@ public final class Settings {
         /** @hide */
         public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_POSITION = "audio_panel_view_position";
+
         /**
          * Whether to disable ripple on lockscreen pin entry
          * @hide
