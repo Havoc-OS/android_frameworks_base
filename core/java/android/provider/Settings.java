1a47dba1e36a73dbcbe2b0373bebc8e890e0cecb
Anushek Prasal <anushekprasal@gmail.com>
2018-12-27 03:00:41 +0530

    Network traffic location [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6239,6 +6239,12 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Network traffic location
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_LOCATION = "network_traffic_location";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
