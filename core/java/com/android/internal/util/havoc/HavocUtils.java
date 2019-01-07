697b1bde88c30ca675bb7e299395c1a3fcfb7b5f
Alex Cruz <du.alexcruz@gmail.com>
2019-01-06 20:00:43 -0600

    Battery saver: Ensure warning doesn't pop up while plugged in
    This doesn't make any sense because if you're plugged in then you don't
    need the battery saver.
    
    - Introduced method to determine if plugged in or not, works with wireless
    charging as well.
    
    Steps to duplicate fail:
    - Clean flash or make sure you've never turned on battery saver
    - Plug in your phone
    - Pull down QS panel
    - Tap on the battery QS tile
    
    Change-Id: Idd9f5382ac68b5af0cf8e3d65c670c2982b62a54

--- core/java/com/android/internal/util/havoc/HavocUtils.java
+++ core/java/com/android/internal/util/havoc/HavocUtils.java
@@ -22,6 +22,7 @@ import android.app.IActivityManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -34,6 +35,7 @@ import android.hardware.SensorManager;
 import android.Manifest;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -387,5 +389,17 @@ public class HavocUtils {
         newColor[2] = empty[2] + ((full[2]-empty[2])*blendFactor);
         int newAlpha = (int) (emptyAlpha + ((fullAlpha-emptyAlpha)*blendFactor));
         return Color.HSVToColor(newAlpha, newColor);
+    }
+
+    // Method to detect if device is plugged in (wired or wireless)
+    public static boolean isPlugged(Context context) {
+        boolean isPlugged;
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        isPlugged = plugged ==
+                BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB;
+        isPlugged = isPlugged || plugged == BatteryManager.BATTERY_PLUGGED_WIRELESS;
+        return isPlugged;
     }
 }
