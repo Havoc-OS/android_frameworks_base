bb19bf40bddadfb4bed885f9e42f345ab3e68a00
Anushek Prasal <anushekprasal@gmail.com>
2019-02-05 15:48:01 +0530

    SystemUI: Add optional notch friendly VoLTE icon
    Since the old icon didn't play too well with some notched devices so let's add a bool for them to use the smaller HD icon for volte.
    
    Icon added from Google Dialer

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1125,6 +1125,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean inflateSignalStrengths = false;
         boolean alwaysShowDataRatIcon = false;
         boolean showVolteIcon;
+        boolean showHDVolteIcon;
         public String patternOfCarrierSpecificDataIcon = "";
 
         /**
@@ -1179,6 +1180,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 }
             }
             config.showVolteIcon = res.getBoolean(R.bool.config_display_volte);
+            config.showHDVolteIcon = res.getBoolean(R.bool.config_display_hd_volte);
             return config;
         }
 
