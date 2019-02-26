610d899a3d75dd9e3518ffff2a6976d0cea201ae
Ali B <abittin@gmail.com>
2019-02-26 13:56:32 +0300

    SystemUI: Update OnTheGo tile
    - cleanup icon handling
    - properly update tile state based on service state
    - declutter bg a bit by collapsing the panel
    
    Change-Id: Ifd593218b5574359fc89331d625255eb866cf8e2

--- packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -25,12 +25,14 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 
 
+import com.android.internal.util.havoc.HavocUtils;
 import com.android.internal.util.havoc.OnTheGoActions;
-import com.android.internal.util.havoc.OnTheGoUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 /** Quick settings tile: OnTheGo Mode **/
 public class OnTheGoTile extends QSTileImpl<BooleanState> {
+
+    private static final String SERVICE_NAME = "com.android.systemui.havoc.onthego.OnTheGoService";
 
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_onthego);
 
@@ -57,6 +59,11 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
+        mHost.collapsePanels();
+        //finish collapsing the panel
+        try {
+             Thread.sleep(1000); //1s
+        } catch (InterruptedException ie) {}
         toggleState();
         refreshState();
     }
@@ -67,24 +74,18 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleLongClick() {
-       // do nothing
-    }
-
-    @Override
     public Intent getLongClickIntent() {
         return null;
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        state.icon = mIcon;
         state.contentDescription =  mContext.getString(
                 R.string.quick_settings_onthego_label);
         state.label = mContext.getString(R.string.quick_settings_onthego_label);
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_onthego);
-        state.state = OnTheGoUtils.isServiceRunning(mContext,
-                "com.android.systemui.havoc.onthego.OnTheGoService")  ?
-                Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+        state.value = HavocUtils.isServiceRunning(mContext, SERVICE_NAME);
+        state.state = state.value ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
     }
 
     @Override
