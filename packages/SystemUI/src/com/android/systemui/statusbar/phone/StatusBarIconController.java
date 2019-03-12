6cadcbefc44705490f1d2638e99a30bc7da96c29
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-03-12 16:01:38 +0100

    NetworkTraffic: Switch to OOS style indicator
    - add separate dimens. values for SBtraffic
    - adjust size and alignment
    - reduce left right padding
    - make traffic text bold/font
    - set gravity to right
    - nuked arrow indicators
    - upload and download speeds view switch depending on which is higher
    
    Change-Id: I48a16dc383ce333f0a64aa87b994d300b26e6a9c
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -315,7 +315,7 @@ public interface StatusBarIconController {
 
         private NetworkTraffic onCreateNetworkTraffic(String slot) {
             NetworkTraffic view = new NetworkTraffic(mContext);
-            view.setPadding(4, 0, 4, 0);
+            view.setPadding(3, 6, 3, 0);
             return view;
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -16,6 +16,8 @@ import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.view.Gravity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.TrafficStats;
@@ -47,7 +49,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private static final int KB = 1024;
     private static final int MB = KB * KB;
     private static final int GB = MB * KB;
-    private static final String symbol = "B/s";
+    private static final String symbol = "/s";
 
     private static DecimalFormat decimalFormat = new DecimalFormat("##0.#");
     static {
@@ -61,7 +63,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private long totalTxBytes;
     private long lastUpdateTime;
     private int txtSize;
-    private int txtImgPadding;
     private int mAutoHideThreshold;
     private int mTintColor;
     private int mVisibleState = -1;
@@ -97,17 +98,26 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
             if (shouldHide(rxData, txData, timeDelta)) {
                 setText("");
                 mTrafficVisible = false;
-            } else {
-                // Get information for uplink ready so the line return can be added
+            } else if (shouldShowUpload(rxData, txData, timeDelta)) {
+                // Show information for uplink if it's called for
                 String output = formatOutput(timeDelta, txData, symbol);
-                // Ensure text size is where it needs to be
-                output += "\n";
+
+                // Update view if there's anything new to show
+                if (!output.contentEquals(getText())) {
+                    setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
+                    setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
+                    setText(output);
+                }
+                mTrafficVisible = true;
+            } else {
                 // Add information for downlink if it's called for
-                output += formatOutput(timeDelta, rxData, symbol);
+                String output = formatOutput(timeDelta, rxData, symbol);
 
                 // Update view if there's anything new to show
                 if (! output.contentEquals(getText())) {
                     setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
+                    setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+		            setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
                     setText(output);
                 }
                 mTrafficVisible = true;
@@ -124,9 +134,9 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
         private String formatOutput(long timeDelta, long data, String symbol) {
             long speed = (long)(data / (timeDelta / 1000F));
             if (speed < KB) {
-                return decimalFormat.format(speed) + symbol;
+                return decimalFormat.format(speed / (float)KB) + 'K' + symbol;
             } else if (speed < MB) {
-                return decimalFormat.format(speed / (float)KB) + 'k' + symbol;
+                return decimalFormat.format(speed / (float)KB) + 'K' + symbol;
             } else if (speed < GB) {
                 return decimalFormat.format(speed / (float)MB) + 'M' + symbol;
             }
@@ -134,12 +144,19 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
         }
 
         private boolean shouldHide(long rxData, long txData, long timeDelta) {
-            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
             long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
+	    long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
             return !getConnectAvailable() ||
                     (speedRxKB < mAutoHideThreshold &&
                     speedTxKB < mAutoHideThreshold);
         }
+
+	private boolean shouldShowUpload(long rxData, long txData, long timeDelta) {
+	    long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
+            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
+
+	    return (speedTxKB > speedRxKB);
+	}
     };
 
     private Runnable mRunnable = new Runnable() {
@@ -195,7 +212,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
         super(context, attrs, defStyle);
         final Resources resources = getResources();
         txtSize = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
-        txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         mTintColor = resources.getColor(android.R.color.white);
         Handler mHandler = new Handler();
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
@@ -288,16 +304,6 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private void updateTrafficDrawable() {
         int intTrafficDrawable;
         if (mIsEnabled) {
-            intTrafficDrawable = R.drawable.stat_sys_network_traffic_updown;
-        } else {
-            intTrafficDrawable = 0;
-        }
-        if (intTrafficDrawable != 0) {
-            Drawable d = getContext().getDrawable(intTrafficDrawable);
-            d.setColorFilter(mTintColor, Mode.MULTIPLY);
-            setCompoundDrawablePadding(txtImgPadding);
-            setCompoundDrawablesWithIntrinsicBounds(null, null, d, null);
-        } else {
             setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         }
     }
@@ -305,9 +311,8 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     public void onDensityOrFontScaleChanged() {
         final Resources resources = getResources();
         txtSize = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
-        txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_multi_text_size);
         setTextSize(TypedValue.COMPLEX_UNIT_PX, (float)txtSize);
-        setCompoundDrawablePadding(txtImgPadding);
+        setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
     }
 
     @Override
