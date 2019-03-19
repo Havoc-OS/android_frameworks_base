77368697353cdef77c1913529dee81d157daa326
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-19 01:26:28 -0300

    ImageHelper: add getBlurredImage
    Bootleggers edit: Modify to use less amount of blur radius, and add a method to put a custom radius

--- core/java/com/android/internal/util/hwkeys/ImageHelper.java
+++ core/java/com/android/internal/util/hwkeys/ImageHelper.java
@@ -50,6 +50,10 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
+import android.renderscript.Element;
+import android.renderscript.Allocation;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.renderscript.RenderScript;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -295,5 +299,29 @@ public class ImageHelper {
             return null;
         }
         return Uri.fromFile(imageFile);
+    }
+
+    public static Bitmap getBlurredImage(Context context, Bitmap image) {
+        return getBlurredImage(context, image, 3.5f);
+    }
+
+    public static Bitmap getBlurredImage(Context context, Bitmap image, float radius) {
+        float BITMAP_SCALE = 0.4f;
+
+        int width = Math.round(image.getWidth() * BITMAP_SCALE);       
+        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);  
+        theIntrinsic.setRadius(radius);
+        theIntrinsic.setInput(tmpIn);
+        theIntrinsic.forEach(tmpOut);
+        tmpOut.copyTo(outputBitmap);
+        return outputBitmap;
     }
 }
