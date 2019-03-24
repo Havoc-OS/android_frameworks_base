4bfb71245d114c34ff1e38a9f9f163dc07eb214f
Alex <du.alexcruz@gmail.com>
2019-03-24 16:02:42 -0500

    Dreamliner: Disable DockObserver conditionally
    This doesn't need to be disabled for all devices, just the Pixel 3/3XL.
    Easiest way to handle this is just via a bool that can be set to true
    in the device tree.
    
    <bool name="config_enableDreamlinerService">true</bool>
    
    - Set to false by default.
    
    Change-Id: I747751f6380dc8dad1ff9e329f2b95ea696843eb

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1344,9 +1344,11 @@ public final class SystemServer {
                 traceEnd();
             }
 
-            traceBeginAndSlog("StartDockObserver");
-            mSystemServiceManager.startService(DockObserver.class);
-            traceEnd();
+            if (!context.getResources().getBoolean(R.bool.config_enableDreamlinerService)) {
+                traceBeginAndSlog("StartDockObserver");
+                mSystemServiceManager.startService(DockObserver.class);
+                traceEnd();
+            }
 
             if (isWatch) {
                 traceBeginAndSlog("StartThermalObserver");
