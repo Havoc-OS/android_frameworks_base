dc5a685b6eb9dfcd347731862f0316abd5286506
Giuseppe Maggio <jertlok@protonmail.com>
2019-03-24 03:07:52 +0100

    Dreamliner: Reorganise the external repo [2/2]
    * Also edit proguard.flags to keep google classes.
    
    Change-Id: Ic6c2412aa773837f1e80228dde26a4b92c5ef59c

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -35,7 +35,7 @@ LOCAL_SRC_FILES := \
     $(call all-Iaidl-files-under, $(RELATIVE_FINGERPRINT_PATH))
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../packages/apps/SmartNav/src)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/google/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/google/systemui/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     SystemUIPluginLib \
@@ -68,7 +68,7 @@ LOCAL_JAVA_LIBRARIES := telephony-common \
     android.car \
     ims-common
 
-LOCAL_FULL_LIBS_MANIFEST_FILES += external/google/GoogleManifest.xml
+LOCAL_FULL_LIBS_MANIFEST_FILES += external/google/systemui/GoogleManifest.xml
 
 LOCAL_PACKAGE_NAME := SystemUI
 LOCAL_PRIVATE_PLATFORM_APIS := true
@@ -78,7 +78,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-keyguard $(LOCAL_PATH)/res $(LOCAL_PATH)/res-hwkeys
 LOCAL_RESOURCE_DIR += packages/apps/SmartNav/res
-LOCAL_RESOURCE_DIR += external/google/res
+LOCAL_RESOURCE_DIR += external/google/systemui/res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
