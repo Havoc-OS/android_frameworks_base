49bc1e1987737d95a0222637591cd9794f15993a
yanglv <yanglv@codeaurora.org>
2019-04-09 16:54:18 +0800

    perfLock: Enable ActivityBoost
    ActivityBoost is removed by mistake, add it back.
    
    CRs-Fixed: 2433998
    
    Change-Id: I9ba5ef8ac3c3d6aef108a57a66402f1fe6240a26
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -3846,6 +3846,9 @@ class ActivityStack<T extends StackWindowController> extends ConfigurationContai
             if (oomAdj) {
                 mService.updateOomAdjLocked();
             }
+
+            r.releasePerfLockHandlerIfNeeded();
+
             return r;
         }
 
