442c2fc80b511e708058242c2b46cf72af236cb9
paphonb <paphonb@gmail.com>
2019-05-06 15:56:46 +0000

    Q Always-On Display: Show media info in keyguard slice
    Change-Id: If45f02374dbc7b1c4c895358d7622cac37a4fc5a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -208,6 +208,7 @@ import com.android.systemui.doze.DozeLog;
 import com.android.systemui.doze.DozeReceiver;
 import com.android.systemui.fragments.ExtensionFragmentListener;
 import com.android.systemui.fragments.FragmentHostManager;
+import com.android.systemui.keyguard.KeyguardSliceProvider;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
@@ -840,6 +841,13 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mColorExtractor = Dependency.get(SysuiColorExtractor.class);
         mColorExtractor.addOnColorsChangedListener(this);
+
+        KeyguardSliceProvider keyguardSliceProvider = KeyguardSliceProvider.getAttachedInstance();
+        if (keyguardSliceProvider != null) {
+            keyguardSliceProvider.setMediaManager(mMediaManager);
+        } else {
+            Log.w("StatusBar", "Cannot init KeyguardSliceProvider dependencies");
+        }
 
 	    mDisplayManager = mContext.getSystemService(DisplayManager.class);
 
@@ -4965,6 +4973,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         mKeyguardIndicationController.setDozing(mDozing);
         mNotificationPanel.setDozing(mDozing, animate);
         mVisualizerView.setDozing(mDozing);
+        KeyguardSliceProvider keyguardSliceProvider = KeyguardSliceProvider.getAttachedInstance();
+        if (keyguardSliceProvider != null) {
+            keyguardSliceProvider.setDozing(mDozing);
+        }
         updateQsExpansionEnabled();
         mKeyguardShortcuts.setDozing(mDozing);
 
@@ -5991,8 +6003,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         mAmbientMediaPlaying = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.FORCE_AMBIENT_FOR_MEDIA, 1,
                 UserHandle.USER_CURRENT) != 0;
-        if (isAmbientContainerAvailable()) {
-            ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(null, false);
+        KeyguardSliceProvider keyguardSliceProvider = KeyguardSliceProvider.getAttachedInstance();
+        if (keyguardSliceProvider != null) {
+            keyguardSliceProvider.setAllowMedia(mAmbientMediaPlaying);
         }
     }
 
