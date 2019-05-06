66e8fdf73562ec9e6c718b1586ec80f781d8f04c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-06 15:51:57 -0300

    LiveDisplay: don't start services if phone is encrypted
    Change-Id: I7d4347fa7dcf3b2d768cb3903aa5c89de373647b
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2014,13 +2014,15 @@ public final class SystemServer {
             mSystemServiceManager.startService(PeopleService.class);
             t.traceEnd();
 
-            t.traceBegin("StartLineageHardwareService");
-            mSystemServiceManager.startService(LineageHardwareService.class);
-            t.traceEnd();
+            if (!mOnlyCore) {
+                t.traceBegin("StartLineageHardwareService");
+                mSystemServiceManager.startService(LineageHardwareService.class);
+                t.traceEnd();
 
-            t.traceBegin("StartLiveDisplayService");
-            mSystemServiceManager.startService(LiveDisplayService.class);
-            t.traceEnd();
+                t.traceBegin("StartLiveDisplayService");
+                mSystemServiceManager.startService(LiveDisplayService.class);
+                t.traceEnd();
+            }
         }
 
         if (!isWatch) {
