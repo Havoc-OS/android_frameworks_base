6e89a6738b49c540f205eda5467f715687fbcc1d
Anushek Prasal <anushekprasal@gmail.com>
2019-05-07 13:58:08 +0530

    Revert "Proximity check on wake"
    Revert "Proximity check on wake fixes"
    
    This reverts commit b53a322d2fd4edaedb55ccad07011124da07db58.
    
    Revert "Ensure proximity check is taken into consideration"
    
    This reverts commit 60e69a8178b5bf577e9d332e04cc34d77c7f5abb.
    
    Revert "PowerManagerService: Log when proximity blocks wake"
    
    This reverts commit 704d39afd3d36c6f674c1ef41aab8a5254123c7c.
    
    Revert "Fix locking for wakeup proximity check"
    
    This reverts commit 2ddfc7f4a188d8688efb10126ad1d32f89d947f2.
    
    Revert "Let InCallUI handle proximity sensor for incoming calls"
    
    This reverts commit ccef47df5a3e68e402fd83a142ed4b716e5a3b99.
    
    Revert "Initialize SensorManager in systemReady()"
    
    This reverts commit 81b22a0e14dbc736bf3328394d39c7be897f181c.
    
    Revert "Add config for proximity check on screen on default value"
    
    This reverts commit 71825ce6515a6baaa8f9d4d09c9d19f3b334c6e3.
    
    Revert "Make proximity check opt-in"
    
    This reverts commit dac106f05a1d7352d65e52d1f437e07f3c755e2e.
    
    Revert "Only use proximity checks on wakeup keys"
    
    This reverts commit 6481e26e76ce0b18e11c1e28a655ff3098efab14.
    
    Revert "Fix proximity check on non power key"
    
    This reverts commit 822f87edce13a601e00e07cc89b128d257d49a42.
    
    Revert "Add support for checking proximity when waking device"
    
    This reverts commit 700ce07b4984b53e59ae2f2a6c2ffbeb212a14f7.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7310,8 +7310,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (isValidGlobalKey(keyCode)
                 && mGlobalKeyManager.shouldHandleGlobalKey(keyCode, event)) {
             if (isWakeKey) {
-                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey,
-                       "android.policy:KEY", true);
+                wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
             }
             return result;
         }
@@ -7756,8 +7755,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (isWakeKey) {
-            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY",
-                    event.getKeyCode() == KeyEvent.KEYCODE_WAKEUP /* check prox only on wake key*/);
+            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY");
         }
 
         return result;
@@ -8243,15 +8241,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void wakeUpFromPowerKey(long eventTime) {
-        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER", true);
+        wakeUp(eventTime, mAllowTheaterModeWakeFromPowerKey, "android.policy:POWER");
     }
 
     private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, String reason) {
-        return wakeUp(wakeTime, wakeInTheaterMode, reason, false);
-    }
-
-    private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, String reason,
-            final boolean withProximityCheck) {
         final boolean theaterModeEnabled = isTheaterModeEnabled();
         if (!wakeInTheaterMode && theaterModeEnabled) {
             return false;
@@ -8262,11 +8255,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.THEATER_MODE_ON, 0);
         }
 
-        if (withProximityCheck) {
-            mPowerManager.wakeUpWithProximityCheck(wakeTime, reason);
-        } else {
-            mPowerManager.wakeUp(wakeTime, reason);
-        }
+        mPowerManager.wakeUp(wakeTime, reason);
         return true;
     }
 
