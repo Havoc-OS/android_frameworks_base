3bdf1dd63917f1fb071c14c257a21b7571a3cd72
Harsh Shandilya <harsh@prjkt.io>
2019-05-13 15:20:32 +0530

    TypographicClock: Tint top row with wallpaper primary color
    Change-Id: Ia4cf09bdef39fc95a910c9aea2c22f043fcd7d7d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- packages/SystemUI/src/com/android/keyguard/clocks/TypographicClock.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/TypographicClock.java
@@ -1,11 +1,13 @@
 package com.android.keyguard.clocks;
 
+import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -17,23 +19,38 @@ import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.android.internal.colorextraction.ColorExtractor;
+import com.android.internal.colorextraction.ColorExtractor.GradientColors;
+import com.android.internal.graphics.ColorUtils;
+import com.android.internal.util.NotificationColorUtil;
 import com.android.keyguard.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.colorextraction.SysuiColorExtractor;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-public class TypographicClock extends TextView {
+public class TypographicClock extends TextView implements ColorExtractor.OnColorsChangedListener {
 
-    private int mAccentColor;
     private String mDescFormat;
     private final String[] mHours;
     private final String[] mMinutes;
     private final Resources mResources;
     private final Calendar mTime;
     private TimeZone mTimeZone;
+    private SysuiColorExtractor mColorExtractor;
+
+    private int mPrimaryColor;
+    private int mAmbientColor;
+    private int mSystemAccent;
+    private int mFallbackColor;
+    private int mCurrentAccent;
+    private float mDarkAmount;
+    private float[] mHslOut = new float[3];
 
     private final BroadcastReceiver mTimeZoneChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -57,12 +74,16 @@ public class TypographicClock extends TextView {
     public TypographicClock(Context context, AttributeSet attributeSet, int defStyleAttr) {
         super(context, attributeSet, defStyleAttr);
 
+        mColorExtractor = Dependency.get(SysuiColorExtractor.class);
+        mColorExtractor.addOnColorsChangedListener(this);
         mTime = Calendar.getInstance(TimeZone.getDefault());
         mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(context)).toLocalizedPattern();
         mResources = context.getResources();
         mHours = mResources.getStringArray(R.array.type_clock_hours);
         mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
-        mAccentColor = mResources.getColor(R.color.custom_text_clock_top_color, null);
+        mSystemAccent = mResources.getColor(R.color.custom_text_clock_top_color, null);
+        mFallbackColor = mResources.getColor(R.color.custom_text_clock_top_fallback_color, null);
+        onColorsChanged(mColorExtractor, 0);
     }
 
     public void onTimeChanged() {
@@ -75,7 +96,7 @@ public class TypographicClock extends TextView {
         SpannableString colored = new SpannableString(rawFormat);
         for (Annotation annotation : annotationArr) {
             if ("color".equals(annotation.getValue())) {
-                colored.setSpan(new ForegroundColorSpan(mAccentColor),
+                colored.setSpan(new ForegroundColorSpan(mCurrentAccent),
                         colored.getSpanStart(annotation),
                         colored.getSpanEnd(annotation),
                         Spanned.SPAN_POINT_POINT);
@@ -89,8 +110,53 @@ public class TypographicClock extends TextView {
         mTime.setTimeZone(timeZone);
     }
 
-    public void setClockColor(int i) {
-        mAccentColor = i;
+    @Override
+    public void onColorsChanged(ColorExtractor extractor, int which) {
+        GradientColors colors = extractor.getColors(WallpaperManager.FLAG_LOCK);
+        setWallpaperColors(colors.getMainColor(), colors.supportsDarkText(), colors.getColorPalette());
+    }
+
+    private void setWallpaperColors(int mainColor, boolean supportsDarkText, int[] colorPalette) {
+        int scrimColor = supportsDarkText ? Color.WHITE : Color.BLACK;
+        int scrimTinted = ColorUtils.setAlphaComponent(ColorUtils.blendARGB(scrimColor, mainColor, 0.5f), 64);
+        int bgColor = ColorUtils.compositeColors(scrimTinted, mainColor);
+
+        int paletteColor = getColorFromPalette(colorPalette);
+        bgColor = ColorUtils.compositeColors(bgColor, Color.BLACK);
+        mPrimaryColor = findColor(paletteColor, bgColor, !supportsDarkText, mSystemAccent, mFallbackColor);
+        mAmbientColor = findColor(paletteColor, Color.BLACK, true, mSystemAccent, mFallbackColor);
+
+        setDarkAmount(mDarkAmount);
+    }
+
+    private int getColorFromPalette(int[] palette) {
+        if (palette != null && palette.length != 0) {
+            return palette[Math.max(0, palette.length - 5)];
+        } else {
+            return mSystemAccent;
+        }
+    }
+
+    private int findColor(int color, int background, boolean againstDark, int accent, int fallback) {
+        if (!isGreyscale(color)) {
+            return color;
+        }
+        int contrastAccent = NotificationColorUtil.ensureTextContrast(accent, background, againstDark);
+        if (!isGreyscale(contrastAccent)) {
+            return contrastAccent;
+        } else {
+            return fallback;
+        }
+    }
+
+    private boolean isGreyscale(int color) {
+        ColorUtils.RGBToHSL(Color.red(color), Color.green(color), Color.blue(color), mHslOut);
+        return mHslOut[1] < 0.1f || mHslOut[2] < 0.1f;
+    }
+
+    public void setDarkAmount(float dark) {
+        mDarkAmount = dark;
+        mCurrentAccent = ColorUtils.blendARGB(mPrimaryColor, mAmbientColor, mDarkAmount);
         onTimeChanged();
     }
 
@@ -119,6 +185,7 @@ public class TypographicClock extends TextView {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         getContext().unregisterReceiver(mTimeZoneChangedReceiver);
+        mColorExtractor.removeOnColorsChangedListener(this);
     }
 
     @Override
