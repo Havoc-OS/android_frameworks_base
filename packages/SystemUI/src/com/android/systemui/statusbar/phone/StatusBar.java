7071a7daa42a267b782bceb5c7c1f722ecdf098b
Pranav Vashi <neobuddy89@gmail.com>
2019-05-14 07:02:48 +0530

    Fix conflict between pulse auto color and lockscreen wallpaper
    * Pulse auto color works only with BitmapDrawable. So create lockscreen
      wallpaper as BitmapDrawable instead LockscreenWallpaperDrawable.
    * If lockscreen wallpaper drawable is not available, we will check system
      wallpaper for pulse autocolor.
    
    Fixes:
    https://github.com/crdroidandroid/android_frameworks_base/commit/d4416f6a5916d847dec62b6a42593177bae1bdd4
    https://github.com/crdroidandroid/android_frameworks_base/commit/c870ab380d0a183b364bd2a89d354659dbe5b8ee
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2034,6 +2034,18 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         }
 
+        boolean allowWhenShade = false;
+        if (ENABLE_LOCKSCREEN_WALLPAPER && artworkDrawable == null) {
+            Bitmap lockWallpaper = mLockscreenWallpaper.getBitmap();
+            if (lockWallpaper != null) {
+                artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), lockWallpaper);
+                // We're in the SHADE mode on the SIM screen - yet we still need to show
+                // the lockscreen wallpaper in that mode.
+                allowWhenShade = mStatusBarKeyguardViewManager != null
+                        && mStatusBarKeyguardViewManager.isShowing();
+            }
+        }
+
         if (artworkDrawable == null && mMediaManager.isMediaPlaying()) {
             //Get wallpaper as bitmap
             WallpaperManager manager = WallpaperManager.getInstance(mContext);
@@ -2046,14 +2058,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 Bitmap lockWallpaper = BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
                 artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), lockWallpaper);
             }
-        }
-
-        boolean allowWhenShade = false;
-        if (ENABLE_LOCKSCREEN_WALLPAPER && mLockscreenWallpaper.getBitmap() != null) {
-            // We're in the SHADE mode on the SIM screen - yet we still need to show
-            // the lockscreen wallpaper in that mode.
-            allowWhenShade = mStatusBarKeyguardViewManager != null
-                    && mStatusBarKeyguardViewManager.isShowing();
         }
 
         boolean hideBecauseOccluded = mStatusBarKeyguardViewManager != null
