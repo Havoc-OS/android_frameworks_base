24c3865d7870e12005e1eb61221c747e193c472f
SuperDroidBond <superdroidbond@yahoo.com>
2019-05-19 23:14:03 +0530

    Battery Estimates: Show only in QSB Header

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -292,7 +292,7 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void updatePercentText() {
         if (mBatteryController != null && mBatteryPercentView != null) {
-            if (!mShowEstimate || mCharging) {
+            if (!mShowEstimate || !misQsbHeader || mCharging) {
                 setPercentTextAtCurrentLevel();
             } else {
                 mBatteryController.getEstimatedTimeRemainingString(this::onEstimateFetchComplete);
