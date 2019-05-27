aa88a4ed48278284c5ebbff585c3f60ad0f7c40b
Kshitij Gupta <kshitijgm@gmail.com>
2019-05-27 21:49:53 +0000

    base: SystemUI: Fix qs-alpha conditions
    - Fixes transparency on custom colored qs.
      Does what 4b71a6b8c03c4e15deb58d7142f2314a555b0d0e tried to do.
    - Make transparency entirely unconditional wrt to the resources check.
    
    Change-Id: Idb03101828fccfe7b79d7898da0b78742000edfa
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -242,18 +242,16 @@ public class QSContainerImpl extends FrameLayout implements
     }
 
     private void setQsBackground() {
-
         if (mSetQsFromResources) {
             mQsBackGround = getContext().getDrawable(R.drawable.qs_background_primary);
-            if (mQsBackGround != null)
-                mQsBackGround.setAlpha(mQsBackGroundAlpha);
         } else {
             if (mQsBackGround != null)
                 mQsBackGround.setColorFilter(mCurrentColor, PorterDuff.Mode.SRC_ATOP);
         }
-        if (mQsBackGround != null && mBackground != null) {
+        if (mQsBackGround != null)
+            mQsBackGround.setAlpha(mQsBackGroundAlpha);
+        if (mQsBackGround != null && mBackground != null)
             mBackground.setBackground(mQsBackGround);
-        }
     }
 
     @Override
