505654572594b1f903362d700942775b42c68e63
DennySPB <dennyspb@gmail.com>
2019-06-18 16:02:14 +0300

    GamingMode: add master switch [1/2]
    if user doesn't want to use gaming mode itself we shouldn't
    check every launching activity for gaming apps
    
    Change-Id: I68961125f7ecf8da3690a56edb8cca0840f4d7ea
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6673,6 +6673,12 @@ public final class Settings {
         public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
 
         /**
+         * Gaming Mode master switch
+         * @hide
+         */
+        public static final String GAMING_MODE_MASTER_SWITCH = "gaming_mode_master_switch";
+
+        /**
          * Save the user defined brightness state
          */
         public static final String GAMING_SCREEN_BRIGHTNESS_MODE = "gaming_screen_brightness_mode";
--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4638,11 +4638,13 @@ public class ActivityManagerService extends IActivityManager.Stub
             }
 
             if (mGamingModeController != null) {
+              if (mGamingModeController.gamingModeMaster()) {
                 if (hostingType.equals("activity")) {
                     if (startResult != null) {
                          mGamingModeController.noteStarted(app.info.packageName);
                     }
                 }
+              }
             }
 
             checkTime(startTime, "startProcess: returned from zygote!");
@@ -21809,7 +21811,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                                         mCompatModePackages.handlePackageUninstalledLocked(ssp);
                                         mBatteryStatsService.notePackageUninstalled(ssp);
                                         if (mGamingModeController != null) {
-                                            mGamingModeController.notePackageUninstalled(ssp);
+                                             mGamingModeController.notePackageUninstalled(ssp);
                                         }
                                     }
                                 } else {
@@ -25304,13 +25306,15 @@ public class ActivityManagerService extends IActivityManager.Stub
 
             if (mCurResumedPackage != null) {
                 if (mGamingModeController != null) {
-                    if(mGamingModeController.topAppChanged(mCurResumedPackage) && !mGamingModeController.getEnabled()) {
-                    Settings.System.putInt(mContext.getContentResolver(),
-                        Settings.System.ENABLE_GAMING_MODE, 1);
-                     } else {
-                        Settings.System.putInt(mContext.getContentResolver(),
-                           Settings.System.ENABLE_GAMING_MODE, 0);
-                     }
+                    if (mGamingModeController.gamingModeMaster()) {
+                        if(mGamingModeController.topAppChanged(mCurResumedPackage) && !mGamingModeController.getEnabled()) {
+                              Settings.System.putInt(mContext.getContentResolver(),
+                                 Settings.System.ENABLE_GAMING_MODE, 1);
+                         } else {
+                              Settings.System.putInt(mContext.getContentResolver(),
+                                 Settings.System.ENABLE_GAMING_MODE, 0);
+                         }
+                    }
                 }
            }
         }
