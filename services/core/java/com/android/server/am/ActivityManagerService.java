b5688217b7f6673bddee65dd0f9b60253675f95b
Pranav Vashi <neobuddy89@gmail.com>
2019-08-04 20:13:25 +0530

    GamingMode: Clean up for dynamic mode
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4637,11 +4637,6 @@ public class ActivityManagerService extends IActivityManager.Stub
                 }
             }
 
-            if (mGamingModeController != null && mGamingModeController.isGamingModeEnabled() &&
-                    hostingType.equals("activity") && startResult != null) {
-                mGamingModeController.noteStarted(app.info.packageName);
-            }
-
             checkTime(startTime, "startProcess: returned from zygote!");
             return startResult;
         } finally {
