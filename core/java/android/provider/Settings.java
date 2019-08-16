2770eb3a172774e74499f1b87d0b76a5f5622b19
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 19:40:55 -0700

    fw/b: [Squash] Add capability to allow tethering to use VPN upstreams
    Squash of:
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 15 19:40:55 2019 -0700
    
        fw/b: Add capability to allow tethering to use VPN upstreams
    
        * Toggled on/off at runtime via a new hotspot lineage setting.
    
        * Dynamically updates the tethering upstream for existing hotspot
          clients as VPNs are brought up / down or the hotspot setting
          is changed.
    
        * This implementation depends on fw/b config_tether_upstream_automatic
          being set to true.
    
        Change-Id: I2ac0b4acc0ea686dfdf54561cb3428808e337160
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 13 16:27:37 2019 -0700
    
        fw/b: Prevent trying to enable hw offload for tethering via VPN upstreams
    
        * Tethering via VPN upstream requires a sw path.
    
        * hw offload setup happened to be being disabled anyway owing to a fail
          return code from setDataLimit().  However, it was causing offload to be
          disabled entirely (until next hotspot off / on event).
    
        * Gracefully skip hw offload for vpn upstreams so that it is automatically
          used again when a vpn is disconnected.
    
        Change-Id: I4df13f02889305560903b7b1e919eedc7af78c07
    
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11272,6 +11272,11 @@ public final class Settings {
         public static final String HARDWARE_KEYS_ENABLE = "hardware_keys_enable";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         */
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
