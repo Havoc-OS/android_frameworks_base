f737079b6d8b20a87eb21b50ffc826f23ac08e2a
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-08-26 07:42:03 +0800

    Fix switch statement paranthesis

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -292,6 +292,7 @@ public class BatteryMeterDrawableBase extends Drawable {
                  return true; break;
             default:
                 return false;
+        }
     }
 
     private void startChargingAnimation(int repeat) {
