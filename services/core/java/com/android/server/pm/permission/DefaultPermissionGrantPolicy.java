707911b486803ebb7df662be5a62a36289b20bc6
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-20 17:47:19 +0530

    Fix permissons for ContactsProvider2
    Fix this error:
    
    W ActivityManager: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    E GooglePlusContactsSync: Failed to clear out contacts
    E GooglePlusContactsSync: java.lang.SecurityException: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    
    [@AgentFabulous - POSP]
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Change-Id: I3544ccb29382ddb693720e9ddead658a77e31847
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -901,6 +901,11 @@ public final class DefaultPermissionGrantPolicy {
         // Google Play Store
         grantSystemFixedPermissionsToSystemPackage(pm,"com.android.vending", userId, CONTACTS_PERMISSIONS,
                 PHONE_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS, SMS_PERMISSIONS, STORAGE_PERMISSIONS);
+
+        // ContactsProvider2
+        grantSystemFixedPermissionsToSystemPackage(pm,
+                getDefaultProviderAuthorityPackage("com.android.providers.contacts.ContactsProvider2", userId), userId,
+                CONTACTS_PERMISSIONS, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
