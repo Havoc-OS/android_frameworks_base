176091f7e987d487df2ee823adb6e59853d1092b
Ali B <abittin@gmail.com>
2019-10-18 21:01:11 +0300

    Network Traffic: remove statusbar animation
    add one remove one: remove padding in the icon controller
    
    Change-Id: Ice77d6517329c0f77ec2e664580d976f1de95c9f
    TODO: find a proper way in StatusIconContainer

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -318,7 +318,6 @@ public interface StatusBarIconController {
 
         private NetworkTrafficSB onCreateNetworkTraffic(String slot) {
             NetworkTrafficSB view = new NetworkTrafficSB(mContext);
-            view.setPadding(4, 0, 4, 0);
             return view;
         }
 
