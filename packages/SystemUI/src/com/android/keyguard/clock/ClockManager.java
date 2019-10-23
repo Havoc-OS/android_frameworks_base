f4c301bd1947c177888159ff01fcaa507cf64bd3
maxwen <max.weninger@gmail.com>
2019-10-24 00:16:43 +0200

    Keyguard: add binary custom clock face
    @neobuddy89: Adapted for Android 11
    
    Change-Id: I92589dd5bd0d284645aaffe7ff40b85a5bc42384
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -162,6 +162,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new DividedLinesClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
