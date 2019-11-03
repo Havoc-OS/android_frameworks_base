b966d7b61fc9e8bec0187a7b02b482d3f112b42e
Dyneteve <dyneteve@pixelexperience.org>
2019-11-03 18:02:03 +0000

    overlay: Fix derp in package name

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -45,7 +45,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedLauncherOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
-	IconPackRoundedThemePickerUIOverlay \
+	IconPackRoundedThemePickerOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
