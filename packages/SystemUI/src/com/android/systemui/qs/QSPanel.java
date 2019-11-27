089d4d8cf9191211bc46fc312f7f7ef3f3544437
maxwen <max.weninger@gmail.com>
2019-11-27 23:48:41 +0100

    SystemUI: fix qs customizer column menu setup
    Change-Id: I7fd44658d64cb058ac073da9cf4c6804cb2614af

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -142,7 +142,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mTileLayout = (QSTileLayout) LayoutInflater.from(mContext).inflate(
                 R.layout.qs_paged_tile_layout, this, false);
         mTileLayout.setListening(mListening);
-        updateSettings();
 
         mQsTileRevealController = new QSTileRevealController(mContext, this,
                 (PagedTileLayout) mTileLayout);
@@ -161,6 +160,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         mIsAutomaticBrightnessAvailable = getResources().getBoolean(
                 com.android.internal.R.bool.config_automatic_brightness_available);
+
+        updateSettings();
     }
 
     private void addQSPanel() {
