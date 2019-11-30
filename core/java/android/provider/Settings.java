9be0ed40afd8eaa5b6618213a118549469c2646c
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    Battery Styles: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    Anushek Prasal <anushekprasal@gmail.com>
    - Convert to SettingsObserver
    - Cleanup tuner code
    
    CarbonROM:
    - Remove broken battery entry from tuner list
    - Add nullcheck for percentage
    
    Dirty Unicorns:
    - Adapt 'updateShowPercent' method to existing modifications
    - Clean up code
    
    Change-Id: I23798426e603b277408fb63f4898b385027e7eeb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4550,10 +4550,7 @@ public final class Settings {
          *    1 - Show percentage
          * @hide
          */
-        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-
-        /** @hide */
-        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = BOOLEAN_VALIDATOR;
+        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
 
         /**
          * Setting to determine whether or not to show the battery percentage in the qs status bar header.
@@ -6311,6 +6308,21 @@ public final class Settings {
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+          * Statusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage inside the icon
+          * 2: Display the battery percentage next to Icon
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6368,7 +6380,6 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
-            SHOW_BATTERY_PERCENT,
             QS_SHOW_BATTERY_ESTIMATE,
             NOTIFICATION_VIBRATION_INTENSITY,
             RING_VIBRATION_INTENSITY,
@@ -6568,7 +6579,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
-            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(QS_SHOW_BATTERY_ESTIMATE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_TEMPERATURE_DAY);
