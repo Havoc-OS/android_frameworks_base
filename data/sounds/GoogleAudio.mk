cdc7321c8b210275dbeeb85bd26080ccc21a2138
Anushek Prasal <anushekprasal@gmail.com>
2019-12-02 21:42:14 +0530

    base: Add more sounds from Pixel

--- data/sounds/GoogleAudio.mk
+++ data/sounds/GoogleAudio.mk
@@ -16,43 +16,102 @@ LOCAL_PATH := frameworks/base/data/sounds
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/google/alarms/A_real_hoot.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/A_real_hoot.ogg \
+    $(LOCAL_PATH)/google/alarms/Argon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Argon.ogg \
+    $(LOCAL_PATH)/google/alarms/Awaken.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Awaken.ogg \
+    $(LOCAL_PATH)/google/alarms/Bounce.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Bounce.ogg \
     $(LOCAL_PATH)/google/alarms/Bright_morning.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Bright_morning.ogg \
+    $(LOCAL_PATH)/google/alarms/Carbon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Carbon.ogg \
     $(LOCAL_PATH)/google/alarms/Cuckoo_clock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Cuckoo_clock.ogg \
+    $(LOCAL_PATH)/google/alarms/Drip.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Drip.ogg \
     $(LOCAL_PATH)/google/alarms/Early_twilight.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Early_twilight.ogg \
+    $(LOCAL_PATH)/google/alarms/Flow.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Flow.ogg \
     $(LOCAL_PATH)/google/alarms/Full_of_wonder.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Full_of_wonder.ogg \
+    $(LOCAL_PATH)/google/alarms/Gallop.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Gallop.ogg \
     $(LOCAL_PATH)/google/alarms/Gentle_breeze.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Gentle_breeze.ogg \
+    $(LOCAL_PATH)/google/alarms/Helium.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Helium.ogg \
     $(LOCAL_PATH)/google/alarms/Icicles.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Icicles.ogg \
     $(LOCAL_PATH)/google/alarms/Jump_start.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Jump_start.ogg \
+    $(LOCAL_PATH)/google/alarms/Krypton.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Krypton.ogg \
     $(LOCAL_PATH)/google/alarms/Loose_change.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Loose_change.ogg \
+    $(LOCAL_PATH)/google/alarms/Neon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Neon.ogg \
+    $(LOCAL_PATH)/google/alarms/Nudge.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Nudge.ogg \
+    $(LOCAL_PATH)/google/alarms/Orbit.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Orbit.ogg \
+    $(LOCAL_PATH)/google/alarms/Osmium.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Osmium.ogg \
+    $(LOCAL_PATH)/google/alarms/Oxygen.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Oxygen.ogg \
+    $(LOCAL_PATH)/google/alarms/Platinum.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Platinum.ogg \
+    $(LOCAL_PATH)/google/alarms/Rise.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Rise.ogg \
     $(LOCAL_PATH)/google/alarms/Rolling_fog.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Rolling_fog.ogg \
     $(LOCAL_PATH)/google/alarms/Spokes.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Spokes.ogg \
     $(LOCAL_PATH)/google/alarms/Sunshower.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Sunshower.ogg \
+    $(LOCAL_PATH)/google/alarms/Sway.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Sway.ogg \
+    $(LOCAL_PATH)/google/alarms/Timer.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Timer.ogg \
+    $(LOCAL_PATH)/google/alarms/Wag.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Wag.ogg \
+    $(LOCAL_PATH)/google/notifications/Ariel.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Ariel.ogg \
     $(LOCAL_PATH)/google/notifications/Beginning.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Beginning.ogg \
+    $(LOCAL_PATH)/google/notifications/Birdsong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Birdsong.ogg \
+    $(LOCAL_PATH)/google/notifications/Carme.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Carme.ogg \
+    $(LOCAL_PATH)/google/notifications/Ceres.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Ceres.ogg \
+    $(LOCAL_PATH)/google/notifications/Chime.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Chime.ogg \
+    $(LOCAL_PATH)/google/notifications/Clink.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Clink.ogg \
     $(LOCAL_PATH)/google/notifications/Coconuts.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Coconuts.ogg \
     $(LOCAL_PATH)/google/notifications/Duet.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Duet.ogg \
+    $(LOCAL_PATH)/google/notifications/Elara.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Elara.ogg \
     $(LOCAL_PATH)/google/notifications/End_note.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/End_note.ogg \
+    $(LOCAL_PATH)/google/notifications/Europa.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Europa.ogg \
+    $(LOCAL_PATH)/google/notifications/Flick.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Flick.ogg \
     $(LOCAL_PATH)/google/notifications/Gentle_gong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Gentle_gong.ogg \
+    $(LOCAL_PATH)/google/notifications/Hey.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Hey.ogg \
+    $(LOCAL_PATH)/google/notifications/Iapetus.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Iapetus.ogg \
+    $(LOCAL_PATH)/google/notifications/Io.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Io.ogg \
     $(LOCAL_PATH)/google/notifications/Mallet.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Mallet.ogg \
+    $(LOCAL_PATH)/google/notifications/Note.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Note.ogg \
     $(LOCAL_PATH)/google/notifications/Orders_up.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Orders_up.ogg \
     $(LOCAL_PATH)/google/notifications/Ping.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Ping.ogg \
     $(LOCAL_PATH)/google/notifications/Pipes.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pipes.ogg \
     $(LOCAL_PATH)/google/notifications/Popcorn.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Popcorn.ogg \
+    $(LOCAL_PATH)/google/notifications/Rhea.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Rhea.ogg \
+    $(LOCAL_PATH)/google/notifications/Salacia.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Salacia.ogg \
     $(LOCAL_PATH)/google/notifications/Shopkeeper.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Shopkeeper.ogg \
     $(LOCAL_PATH)/google/notifications/Sticks_and_stones.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Sticks_and_stones.ogg \
+    $(LOCAL_PATH)/google/notifications/Strum.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Strum.ogg \
+    $(LOCAL_PATH)/google/notifications/Tethys.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Tethys.ogg \
+    $(LOCAL_PATH)/google/notifications/Titan.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Titan.ogg \
+    $(LOCAL_PATH)/google/notifications/Trill.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Trill.ogg \
     $(LOCAL_PATH)/google/notifications/Tuneup.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Tuneup.ogg \
     $(LOCAL_PATH)/google/notifications/Tweeter.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Tweeter.ogg \
     $(LOCAL_PATH)/google/notifications/Twinkle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Twinkle.ogg \
+    $(LOCAL_PATH)/google/ringtones/Atria.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Atria.ogg \
+    $(LOCAL_PATH)/google/ringtones/Beats.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Beats.ogg \
+    $(LOCAL_PATH)/google/ringtones/Callisto.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Callisto.ogg \
     $(LOCAL_PATH)/google/ringtones/Copycat.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Copycat.ogg \
     $(LOCAL_PATH)/google/ringtones/Crackle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Crackle.ogg \
+    $(LOCAL_PATH)/google/ringtones/Dance_party.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Dance_party.ogg \
+    $(LOCAL_PATH)/google/ringtones/Dione.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Dione.ogg \
+    $(LOCAL_PATH)/google/ringtones/Early_bird.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Early_bird.ogg \
     $(LOCAL_PATH)/google/ringtones/Flutterby.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Flutterby.ogg \
+    $(LOCAL_PATH)/google/ringtones/Ganymede.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Ganymede.ogg \
+    $(LOCAL_PATH)/google/ringtones/Hey_hey.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Hey_hey.ogg \
     $(LOCAL_PATH)/google/ringtones/Hotline.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Hotline.ogg \
     $(LOCAL_PATH)/google/ringtones/Leaps_and_bounds.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Leaps_and_bounds.ogg \
     $(LOCAL_PATH)/google/ringtones/Lollipop.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Lollipop.ogg \
     $(LOCAL_PATH)/google/ringtones/Lost_and_found.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Lost_and_found.ogg \
+    $(LOCAL_PATH)/google/ringtones/Luna.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Luna.ogg \
     $(LOCAL_PATH)/google/ringtones/Mash_up.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Mash_up.ogg \
     $(LOCAL_PATH)/google/ringtones/Monkey_around.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Monkey_around.ogg \
+    $(LOCAL_PATH)/google/ringtones/Oberon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Oberon.ogg \
+    $(LOCAL_PATH)/google/ringtones/Phobos.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Phobos.ogg \
+    $(LOCAL_PATH)/google/ringtones/Pyxis.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Pyxis.ogg \
+    $(LOCAL_PATH)/google/ringtones/Rrrring.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Rrrring.ogg \
+    $(LOCAL_PATH)/google/ringtones/Sedna.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Sedna.ogg \
     $(LOCAL_PATH)/google/ringtones/Schools_out.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Schools_out.ogg \
+    $(LOCAL_PATH)/google/ringtones/Shooting_star.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Shooting_star.ogg \
+    $(LOCAL_PATH)/google/ringtones/Spaceship.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Spaceship.ogg \
+    $(LOCAL_PATH)/google/ringtones/Summer_night.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Summer_night.ogg \
     $(LOCAL_PATH)/google/ringtones/The_big_adventure.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/The_big_adventure.ogg \
+    $(LOCAL_PATH)/google/ringtones/Titania.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Titania.ogg \
+    $(LOCAL_PATH)/google/ringtones/Triton.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Triton.ogg \
+    $(LOCAL_PATH)/google/ringtones/Umbriel.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Umbriel.ogg \
+    $(LOCAL_PATH)/google/ringtones/Zen.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Zen.ogg \
     $(LOCAL_PATH)/google/ringtones/Zen_too.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Zen_too.ogg \
     $(LOCAL_PATH)/google/ui/audio_end.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/audio_end.ogg \
     $(LOCAL_PATH)/google/ui/audio_initiate.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/audio_initiate.ogg \
@@ -80,4 +139,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/google/ui/VideoRecord.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/VideoRecord.ogg \
     $(LOCAL_PATH)/google/ui/VideoStop.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/VideoStop.ogg \
     $(LOCAL_PATH)/google/ui/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/WirelessChargingStarted.ogg
-
