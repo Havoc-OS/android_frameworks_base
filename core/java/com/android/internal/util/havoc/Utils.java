56c02a01e79b274591c30a7322c1351203396f47
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-12-31 21:01:50 +0100

    Utils: Add a method to enable/disable package components
    Change-Id: If4b176409323f9b919a65e10d9fc41236f2c5894

--- core/java/com/android/internal/util/havoc/Utils.java
+++ core/java/com/android/internal/util/havoc/Utils.java
@@ -18,6 +18,7 @@ package com.android.internal.util.havoc;
 
 import android.Manifest;
 import android.app.UiModeManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -262,5 +263,13 @@ public class Utils {
         int mode = mUiModeManager.getNightMode();
         return (mode == UiModeManager.MODE_NIGHT_YES);
     }
-}
 
+    public static void setComponentState(Context context, String packageName,
+            String componentClassName, boolean enabled) {
+        PackageManager pm  = context.getApplicationContext().getPackageManager();
+        ComponentName componentName = new ComponentName(packageName, componentClassName);
+        int state = enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        pm.setComponentEnabledSetting(componentName, state, PackageManager.DONT_KILL_APP);
+    }
+}
