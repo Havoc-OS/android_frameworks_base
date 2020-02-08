8f572f97c8cf0a7bcf3adf552c49a37a97351eb0
Jon West <electrikjesus@gmail.com>
2020-02-08 15:06:39 -0500

    Add minimize & pip buttons to freeform windows
    Thanks to farmerbb for assistance and RadekBledowski for the graphics
    
    Change-Id: I8ddad6eab27f52574ee1c10e8006612fb46a83c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/view/Window.java
+++ core/java/android/view/Window.java
@@ -663,6 +663,8 @@ public abstract class Window {
          * Update the navigation bar color to a forced one.
          */
         void updateNavigationBarColor(int color);
+
+        boolean moveTaskToBack(boolean nonRoot);
     }
 
     /** @hide */
