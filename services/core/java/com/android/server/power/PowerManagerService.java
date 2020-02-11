3c7383cdf4e0431d12a9344ff5d8855738612854
maxwen <max.weninger@gmail.com>
2020-02-11 13:31:04 +0100

    base: cleanup notification pulse values
    a few unneded and a few with wrong names
    
    Change-Id: I755282a0a7d7d7d1f556d10ac8fbb8af05447909
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1063,7 +1063,7 @@ public final class PowerManagerService extends SystemService
         Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.DOZE_ON_CHARGE_NOW, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(mContext.getContentResolver(),
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0, UserHandle.USER_CURRENT);
+                Settings.System.AOD_NOTIFICATION_PULSE_ACTIVATED, 0, UserHandle.USER_CURRENT);
 
         synchronized (mLock) {
             mSystemReady = true;
@@ -1183,10 +1183,10 @@ public final class PowerManagerService extends SystemService
                 Settings.Secure.DOZE_ON_CHARGE),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT),
+                Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER),
                 false, mSettingsObserver, UserHandle.USER_ALL);
         resolver.registerContentObserver(Settings.System.getUriFor(
-                Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED),
+                Settings.System.AOD_NOTIFICATION_PULSE),
                 false, mSettingsObserver, UserHandle.USER_ALL);
 
         IVrManager vrManager = IVrManager.Stub.asInterface(getBinderService(Context.VR_SERVICE));
@@ -1322,21 +1322,20 @@ public final class PowerManagerService extends SystemService
         Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.DOZE_ON_CHARGE_NOW, mDozeOnChargeEnabled && mIsPowered ? 1 : 0,
                 UserHandle.USER_CURRENT);
-        boolean mAmbientLights = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
-                0, UserHandle.USER_CURRENT) != 0;
+        boolean mAmbientLights = Settings.System.getIntForUser(resolver,
+                Settings.System.AOD_NOTIFICATION_PULSE, 0, UserHandle.USER_CURRENT) != 0;
         if (mAmbientLights) {
             boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
-            Settings.System.putIntForUser(mContext.getContentResolver(),
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, dozeOnNotification ? 1 : 0,
+                    Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0, UserHandle.USER_CURRENT) != 0;
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.AOD_NOTIFICATION_PULSE_ACTIVATED, dozeOnNotification ? 1 : 0,
                     UserHandle.USER_CURRENT);
         } else {
-            Settings.System.putIntForUser(mContext.getContentResolver(),
-                    Settings.System.AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, 0,
+             Settings.System.putIntForUser(resolver,
+                    Settings.System.AOD_NOTIFICATION_PULSE_ACTIVATED, 0,
                     UserHandle.USER_CURRENT);
         }
-        // depends on AMBIENT_NOTIFICATION_LIGHT_ACTIVATED - so MUST be afterwards
+        // depends on AOD_NOTIFICATION_PULSE_ACTIVATED - so MUST be afterwards
         // no need to call us again
         mAlwaysOnEnabled = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
 
