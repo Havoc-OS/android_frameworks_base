1db2618bfedb82a4f2922b849284eda3cfb4e8a2
Anushek Prasal <anushekprasal@gmail.com>
2020-02-24 03:14:26 +0530

    FODCircleView: Increase dreaming icon alpha

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -390,7 +390,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
         if (mIsCircleShowing) {
             setAlpha(1.0f);
         } else {
-            setAlpha(mIsDreaming ? 0.5f : 1.0f);
+            setAlpha(mIsDreaming ? 0.75f : 1.0f);
         }
     }
 
