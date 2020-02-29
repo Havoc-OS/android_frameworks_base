4baf6953b08c163bd8c46d51e9e5e6e27485e228
ezio84 <brabus84@gmail.com>
2020-02-29 19:58:39 +0100

    Allow a wider back swipe to trigger actions (instead of timeout) [1/2]
    swipe for more than 3/4 of X axis display size to trigger action
    instead of long swipe timeout
    
    Change-Id: Id96affb3df85adafccfc368f11b204469cc3b424

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4251,6 +4251,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BACK_SWIPE_EXTENDED),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
