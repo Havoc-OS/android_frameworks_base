e5f52ea40da547ad2d30217bd194a01fb1d42128
ezio84 <brabus84@gmail.com>
2020-03-01 10:30:39 +0100

    Add kill app action to nav swipe actions [1/3]
    Change-Id: I03b3cfbbe20133982fc04c32470f2901abba97bf

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -673,6 +673,9 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 12: // Ringer modes
                 ActionUtils.toggleRingerModes(mContext);
                 break;
+            case 13: // Kill app
+                ActionUtils.killForegroundApp();
+                break;
         }
     }
 
