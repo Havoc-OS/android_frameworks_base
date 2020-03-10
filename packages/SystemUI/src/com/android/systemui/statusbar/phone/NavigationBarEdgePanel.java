9b44374566274cdfaca03620013ac2a8a538e13a
Nico <nicorg2529@gmail.com>
2020-03-10 21:53:40 +0100

    Gesture nav extended swipe: show a double arrow when almost triggered
    abcdu: adapted to our code

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarEdgePanel.java
@@ -133,6 +133,8 @@ public class NavigationBarEdgePanel extends View {
     private final float mBaseTranslation;
     private final float mArrowLength;
     private final float mArrowThickness;
+    private float mLongSwipeThreshold;
+    private boolean mTriggerLongSwipe;
 
     /**
      * The minimum delta needed in movement for the arrow to change direction / stop triggering back
@@ -413,6 +415,7 @@ public class NavigationBarEdgePanel extends View {
         mVelocityTracker.addMovement(event);
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN : {
+                mTriggerLongSwipe = false;
                 mDragSlopPassed = false;
                 resetOnDown();
                 mStartX = event.getX();
@@ -462,6 +465,9 @@ public class NavigationBarEdgePanel extends View {
         float x = (polarToCartX(mCurrentAngle) * mArrowLength);
         float y = (polarToCartY(mCurrentAngle) * mArrowLength);
         Path arrowPath = calculatePath(x,y);
+        if (mTriggerLongSwipe) {
+            arrowPath.addPath(calculatePath(x,y), mArrowThickness * 2.0f * (mIsLeftPanel ? 1 : -1), 0.0f);
+        }
         if (mShowProtection) {
             canvas.drawPath(arrowPath, mProtectionPaint);
         }
@@ -482,6 +488,7 @@ public class NavigationBarEdgePanel extends View {
                 R.dimen.navigation_edge_panel_padding);
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         mScreenSize = Math.min(metrics.widthPixels, metrics.heightPixels);
+        mLongSwipeThreshold = metrics.widthPixels * 0.45f;
     }
 
     private void updateArrowDirection() {
@@ -625,6 +632,7 @@ public class NavigationBarEdgePanel extends View {
         float x = event.getX();
         float y = event.getY();
         float touchTranslation = MathUtils.abs(x - mStartX);
+        mTriggerLongSwipe = touchTranslation > mLongSwipeThreshold;
         float yOffset = y - mStartY;
         float delta = touchTranslation - mPreviousTouchTranslation;
         if (Math.abs(delta) > 0) {
