6913b54a2d2af3511d6c0a5d0343f47afb84675b
rituj <ritujbeniwal@gmail.com>
2020-03-25 16:13:51 +0530

    SystemUI: Keyguard: Make space for fod circle below pattern/pin view
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Ie9652afc0d105199bf1e660221c82266ac8cf30a
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainer.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSecurityContainer.java
@@ -23,6 +23,8 @@ import static android.view.WindowInsetsAnimation.Callback.DISPATCH_MODE_STOP;
 
 import static com.android.systemui.DejankUtils.whitelistIpcs;
 
+import static com.android.systemui.util.Utils.getFODHeight;
+
 import static java.lang.Integer.max;
 
 import android.animation.Animator;
@@ -514,11 +516,8 @@ public class KeyguardSecurityContainer extends FrameLayout implements KeyguardSe
 
     @Override
     public WindowInsets onApplyWindowInsets(WindowInsets insets) {
-
         // Consume bottom insets because we're setting the padding locally (for IME and navbar.)
         int inset;
-        int minBottomMargin = getResources().getDimensionPixelSize(
-                R.dimen.kg_security_container_min_bottom_margin);
 
         if (sNewInsetsMode == NEW_INSETS_MODE_FULL) {
             int bottomInset = insets.getInsetsIgnoringVisibility(systemBars()).bottom;
@@ -527,8 +526,9 @@ public class KeyguardSecurityContainer extends FrameLayout implements KeyguardSe
         } else {
             inset = insets.getSystemWindowInsetBottom();
         }
-        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(),
-               minBottomMargin > inset ? minBottomMargin : inset);
+        inset = mUpdateMonitor.isUnlockWithFingerprintPossible() ?
+                max(getFODHeight(mContext, false), inset) : inset;
+        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(), inset);
         return insets.inset(0, 0, 0, inset);
     }
 
