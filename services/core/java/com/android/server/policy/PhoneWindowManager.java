d0183aae251b1db75537c3730c8487bcc390a3e8
Alex Cruz <du.alexcruz@gmail.com>
2020-04-27 10:46:08 -0400

    ActionUtils: postdelay screenshot action
    Change-Id: I31ce03fb1aea61af307a66cc88b74e06d46561a2

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6461,7 +6461,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         TAG + "sendCustomAction permission denied");
                 mHandler.removeCallbacks(mScreenshotRunnable);
                 mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
-                mHandler.post(mScreenshotRunnable);
+                mHandler.postDelayed(mScreenshotRunnable, ViewConfiguration.get(mContext).getDeviceGlobalActionKeyTimeout());
             } else if (ActionUtils.INTENT_REGION_SCREENSHOT.equals(action)) {
                 mContext.enforceCallingOrSelfPermission(Manifest.permission.ACCESS_SURFACE_FLINGER,
                         TAG + "sendCustomAction permission denied");
