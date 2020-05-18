d5efe886a0cbf304ac355d0a51cbe135147cf4bf
Weijie Wang <weijiew@codeaurora.org>
2020-05-18 19:25:54 +0800

    SystemUI: support VoWIFI icons
    Show VoWIFI icon when VoWIFI is available
    Show VoWIFI call icon when voice call is over WIFI
    
    CRs-Fixed: 2689484
    Change-Id: Id5b8ea61d8dbf129f17bd6e9cd0f39b4bc440506
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -160,9 +160,14 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         } else {
             mMobileGroup.setVisibility(View.VISIBLE);
         }
-        mMobileDrawable.setLevel(mState.strengthId);
-
         boolean showRoamingSpace = false;
+        if (mState.strengthId > 0) {
+            mMobile.setVisibility(View.VISIBLE);
+            mMobileDrawable.setLevel(mState.strengthId);
+        } else {
+            mMobile.setVisibility(View.GONE);
+        }
+
         if (mState.typeId > 0) {
             if (mOldStyleType) {
                 showOldStyle(mState);
@@ -216,8 +221,11 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
             mMobileGroup.setVisibility(state.visible ? View.VISIBLE : View.GONE);
             needsLayout = true;
         }
-        if (mState.strengthId != state.strengthId) {
+        if (state.strengthId > 0) {
             mMobileDrawable.setLevel(state.strengthId);
+            mMobile.setVisibility(View.VISIBLE);
+        } else {
+            mMobile.setVisibility(View.GONE);
         }
         boolean showRoamingSpace = false;
         if (mState.typeId != state.typeId) {
