0b89c7b20033174e87e25e2bae59660a6de1a867
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-05 22:21:04 +0300

    base: Allow choosing a custom vibration pattern [1/3]
    Change-Id: I0d00f94b185a1032deb3e8413720d47a411cd6f7
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5156,9 +5156,17 @@ public final class Settings {
          * 2: mm-mm-mm
          * 3: da-da-dzzz
          * 4: da-dzzz-da
+         * 5: custom
          * @hide
          */
         public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+
+        /**
+         * Custom vibration pattern
+         * format: ms,ms,ms each a range from 0 to 1000 ms
+         * @hide
+         */
+        public static final String CUSTOM_RINGTONE_VIBRATION_PATTERN = "custom_ringtone_vibration_pattern";
 
         /**
          * How many rows to show in the qs panel when in portrait
