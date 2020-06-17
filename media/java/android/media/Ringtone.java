553656cf2122de2ecbd5ca48666eb84e39213150
Pranav Vashi <neobuddy89@gmail.com>
2020-06-17 20:55:51 +0530

    Opt-out ringtone audio focus for Wired/BT headset [1/2]
    * This is not supported by majority of devices.
      Devices can check and enable using config overlay.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- media/java/android/media/Ringtone.java
+++ media/java/android/media/Ringtone.java
@@ -99,6 +99,8 @@ public class Ringtone {
     private float mVolume = 1.0f;
     private final Object mPlaybackSettingsLock = new Object();
 
+    private final boolean mDeviceRingtoneFocus;
+
     /** {@hide} */
     @UnsupportedAppUsage
     public Ringtone(Context context, boolean allowRemote) {
@@ -107,6 +109,8 @@ public class Ringtone {
         mAllowRemote = allowRemote;
         mRemotePlayer = allowRemote ? mAudioManager.getRingtonePlayer() : null;
         mRemoteToken = allowRemote ? new Binder() : null;
+        mDeviceRingtoneFocus = mContext.getResources().getBoolean(
+            com.android.internal.R.bool.config_deviceRingtoneFocusMode);
     }
 
     /**
@@ -372,8 +376,12 @@ public class Ringtone {
         mLocalPlayer = new MediaPlayer();
         try {
             mLocalPlayer.setDataSource(mContext, mUri);
-            setCustomAudioAttributes();
-            mLocalPlayer.setAudioAttributes(mFinalAudioAttributes);
+            if (mDeviceRingtoneFocus) {
+                setCustomAudioAttributes();
+                mLocalPlayer.setAudioAttributes(mFinalAudioAttributes);
+            } else {
+                mLocalPlayer.setAudioAttributes(mAudioAttributes);
+            }
             synchronized (mPlaybackSettingsLock) {
                 applyPlaybackProperties_sync();
             }
