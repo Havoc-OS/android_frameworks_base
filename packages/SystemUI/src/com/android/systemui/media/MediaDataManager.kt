c62843bbc299a160364e8d71765a0f955b3eeb9d
Beth Thibodeau <ethibodeau@google.com>
2020-08-07 11:40:27 -0400

    base: Allow user to block individual apps from resuming
    This adds a setting which stores a list of packages that will be
    prevented from persisting in QS as resumable media controls, even when
    resumption is enabled. If the user adds a new package to this list when
    it already has a resume control, that control will be removed.
    
    wave notes:
    - this was added then reverted in QPR1 f7159102935b8befda9d62ae3d5cdb9c02ec7223
    - reason was specified as "no longer want change in QPR"
    - but this is added in S preview 1 and works fine so lets bring it back
    
    Bug: 161813143
    Test: manual, atest
    Change-Id: I8c85bc937aeaf366954f2669eba8f6954640fe4c
    Merged-In: I8c85bc937aeaf366954f2669eba8f6954640fe4c
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/MediaDataManager.kt
+++ packages/SystemUI/src/com/android/systemui/media/MediaDataManager.kt
@@ -57,6 +57,8 @@ import java.io.PrintWriter
 import java.util.concurrent.Executor
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.collections.ArrayList
+import kotlin.collections.LinkedHashMap
 
 // URI fields to try loading album art from
 private val ART_URIS = arrayOf(
@@ -117,6 +119,17 @@ class MediaDataManager(
     // TODO(b/159539991#comment5): Move internal listeners to separate package.
     private val internalListeners: MutableSet<Listener> = mutableSetOf()
     private val mediaEntries: LinkedHashMap<String, MediaData> = LinkedHashMap()
+    internal var appsBlockedFromResume: MutableSet<String> = Utils.getBlockedMediaApps(context)
+        set(value) {
+            // Update list
+            appsBlockedFromResume.clear()
+            appsBlockedFromResume.addAll(value)
+
+            // Remove any existing resume players that are now blocked
+            appsBlockedFromResume.forEach {
+                removeAllForPackage(it)
+            }
+        }
 
     @Inject
     constructor(
@@ -610,7 +623,8 @@ class MediaDataManager(
     fun onNotificationRemoved(key: String) {
         Assert.isMainThread()
         val removed = mediaEntries.remove(key)
-        if (useMediaResumption && removed?.resumeAction != null) {
+        if (useMediaResumption && removed?.resumeAction != null &&
+                !isBlockedFromResume(removed?.packageName)) {
             if (DEBUG) Log.d(TAG, "Not removing $key because resumable")
             // Move to resume key (aka package name) if that key doesn't already exist.
             val resumeAction = getResumeMediaAction(removed.resumeAction!!)
@@ -636,6 +650,13 @@ class MediaDataManager(
         if (removed != null) {
             notifyMediaDataRemoved(key)
         }
+    }
+
+    private fun isBlockedFromResume(packageName: String?): Boolean {
+        if (packageName == null) {
+            return true
+        }
+        return appsBlockedFromResume.contains(packageName)
     }
 
     fun setMediaResumptionEnabled(isEnabled: Boolean) {
@@ -695,6 +716,7 @@ class MediaDataManager(
             println("externalListeners: ${mediaDataFilter.listeners}")
             println("mediaEntries: $mediaEntries")
             println("useMediaResumption: $useMediaResumption")
+            println("appsBlockedFromResume: $appsBlockedFromResume")
         }
     }
 }
