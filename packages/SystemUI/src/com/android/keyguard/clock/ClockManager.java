6f0053d850a7a28d856058a79d8e4e17fe50ec16
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-08-15 22:38:44 +0530

    Introduce Fluid lockscreen clock
    AshutoshSundresh:
    -Move string to controller instead of xml as it isn't translatable anyways
    -Port to A11
    -Prevent it from getting stuck on Ambient Display
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I7e2a33797a49e8f291ad255bb60d63aca36f9bfb
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -157,6 +157,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
