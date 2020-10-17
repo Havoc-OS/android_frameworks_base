59cc872ea1dcbfd13fd84f83c897e3fdaa20f060
Marko Man <darkobas@gmail.com>
2020-10-17 21:33:04 +0200

    [1/2] SystemUI: Add custom color to ambient pulse notification lights
    Add custom color to ambient pulse notification lights
    
    Change-Id: Ie67310221c0a100497a9d36847915d52e3854de1
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5428,6 +5428,18 @@ public final class Settings {
         public static final String AOD_NOTIFICATION_PULSE_TIMEOUT = "ambient_notification_light_timeout";
 
         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_ACCENT = "ambient_notification_light_accent";
+
+        /**
+         * Pulse Ambient lights color
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
