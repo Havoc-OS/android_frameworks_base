83091178b75e6a1f8e036755d34ded334a313d95
DennySPb <dennyspb@gmail.com>
2020-11-03 14:49:54 +0300

    base: Legacy WFD output video mode settings [1/3]
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I57fed841d91f2c48f070e154538a58cd98116ed6
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6201,6 +6201,11 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         * @hide
+         */
+        public static final String WFD_LEGACY_VIDEO_MODE = "wfd_legacy_video_mode";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
