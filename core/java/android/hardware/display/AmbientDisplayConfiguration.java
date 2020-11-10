2f88ac3d391eefd7590c2293fd8c529078e0175e
ezio84 <brabus84@gmail.com>
2020-11-10 21:50:35 +0100

    Fix 2tap2wake after Ambient Pulsing on some devices
    like taimen and walleye, instead sunfish (and probably newer pixels)
    doesn't need this
    
    To apply, override the config_has_weird_dt_sensor bool in the device tree
    
    ----
    TL;DR
    for some reason, on taimen and walleye, after ambient pulsing
    gets triggered by adb with the official "com.android.systemui.doze.pulse"
    intent or by our custom "wake to ambient" features, the double tap
    sensor dies if you follow this steps:
    - screen is OFF
    - trigger ambient pulsing with a double tap to wake (if custom wake to
      ambient feature is enabled), or the official intent by adb, or with
      music ticker or any other event
    - after ambient display shows up, don't touch anything and wait till the
      screen goes OFF again
    - double tap to wake, again
    - the double tap sensor doesn't work at all and device doesn't wake up
    
    Now, funny thing, after the steps above, if you cover then uncover the
    proximity/brightness sensor with the hand, then double tap to wake
    again, the wake gesture works as expected.
    
    When covering/uncovering the proximity/brightness sensor, this happens:
    11-10 22:02:00.916   967   998 I ASH     : @ 1993.460: ftm4_disable_sensor: disabling sensor [double-tap]
    11-10 22:02:02.013   967   998 I ASH     : @ 1994.556: ftm4_enable_sensor: enabling sensor [double-tap]
    
    When you switch screen ON with power button, the doze screen states do
    the same: the sensor gets disabled then enabled again if device goes
    to DOZE idle state.
    
    Instead, after Ambient pulsing, when the pulsing finishes, the sensor
    is still enabled, so the disable/enable event doesn't happen this
    time. And that's why, for some reason, it doesn't respond anymore.
    ----
    
    So, in a nutshell: i've no idea why this sh#t happens lol,
    but with a super lazy hacky tricky dirty bloody nooby line change,
    we can force the sensor disable/enable event when the device goes
    to DOZE state.
    
    Change-Id: I8ce463a6e435e540e3ca93336c5dba7a95771b56
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -36,6 +36,7 @@ public class AmbientDisplayConfiguration {
     private final Context mContext;
     private final boolean mAlwaysOnByDefault;
     private final boolean mDeviceHasSoli;
+    private final boolean mDeviceWithWeirdDtSensor;
 
     /** {@hide} */
     @TestApi
@@ -43,6 +44,7 @@ public class AmbientDisplayConfiguration {
         mContext = context;
         mAlwaysOnByDefault = mContext.getResources().getBoolean(R.bool.config_dozeAlwaysOnEnabled);
         mDeviceHasSoli = mContext.getResources().getBoolean(R.bool.config_has_Soli);
+        mDeviceWithWeirdDtSensor = mContext.getResources().getBoolean(R.bool.config_has_weird_dt_sensor);
     }
 
     /** {@hide} */
@@ -244,5 +246,10 @@ public class AmbientDisplayConfiguration {
     public boolean isAmbientGestureEnabled(int user) {
         return !mDeviceHasSoli && Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.AMBIENT_WAKE_GESTURES, 1, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean deviceHasWeirtdDtSensor() {
+        return mDeviceWithWeirdDtSensor;
     }
 }
