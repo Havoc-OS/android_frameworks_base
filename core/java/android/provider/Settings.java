206871dce10a3270d91bb436f8761b1c9876d842
calebcabob <calphonic@gmail.com>
2020-12-10 15:19:04 -0500

    base: Allow enabling/disabling advanced reboot in powermenu [1/2]
    Something similar to this can be done for customizing
    the power menu as well (add/remove items).
    
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Change-Id: I9b58abcef9d88fbec6592fa31760c5d9d6ebeebc
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5276,6 +5276,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Whether to show Advanced action in powermenu or not
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
