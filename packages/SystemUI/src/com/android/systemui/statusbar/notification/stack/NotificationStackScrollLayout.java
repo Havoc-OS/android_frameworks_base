523be1205b9a501a3cb3976adb08c0b4039406c8
Ido Ben-Hur <idoybh2@gmail.com>
2020-12-10 04:18:45 +0200

    base: Fix edge lights when sensitive notification content is hidden on keyguard
    Change-Id: I9bcd0df5c9492da43046f04b6a675e00efe20c50
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -814,7 +814,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
                 continue;
             }
             final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
-            if (row.canViewBeDismissed() && matchesSelection(row, selection)) {
+            if (row.getEntry().isClearable() && matchesSelection(row, selection)) {
                 return row;
             }
         }
