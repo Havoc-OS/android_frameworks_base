f3c59183c0ea3bd9728691b1f5671c1d74793adc
ezio84 <brabus84@gmail.com>
2020-12-27 23:52:54 +0100

    SystemUIGoogle port: don't override aosp scrim controller
    redfin livewallpapers scrim controller breaks AoD
    to replicate the issue:
    - enable AoD (without lift to wake)
    - cover the proximity sensor and wait till AoD gets paused and screen
      goes off
    - uncover the sensor
    - screen is still black (it's not really OFF, indeed AoD gets
      started correctly and screen becomes active - dozeservice debugging
    can show this - )
    
    Change-Id: I341d58984d51dbe4e94f333c809ec0a29e46e6f5
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/statusbar/phone/StatusBarGoogle.java
@@ -71,6 +71,7 @@ import com.android.systemui.statusbar.phone.LockscreenWallpaper;
 import com.android.systemui.statusbar.phone.NotificationGroupManager;
 import com.android.systemui.statusbar.phone.NotificationShadeWindowController;
 import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy;
+import com.android.systemui.statusbar.phone.ScrimController;
 import com.android.systemui.statusbar.phone.ShadeController;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
@@ -91,7 +92,6 @@ import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.volume.VolumeComponent;
 
-import com.google.android.systemui.LiveWallpaperScrimController;
 import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
 import com.google.android.systemui.smartspace.SmartSpaceController;
 
@@ -162,7 +162,7 @@ public class StatusBarGoogle extends StatusBar {
             NotificationShadeWindowController notificationShadeWindowController,
             LockscreenLockIconController lockscreenLockIconController,
             DozeParameters dozeParameters,
-            LiveWallpaperScrimController liveWallpaperScrimController,
+            ScrimController scrimController,
             @Nullable KeyguardLiftController keyguardLiftController,
             Lazy<LockscreenWallpaper> lockscreenWallpaperLazy,
             Lazy<BiometricUnlockController> biometricUnlockControllerLazy,
@@ -242,7 +242,7 @@ public class StatusBarGoogle extends StatusBar {
                 notificationShadeWindowController,
                 lockscreenLockIconController,
                 dozeParameters,
-                liveWallpaperScrimController,
+                scrimController,
                 keyguardLiftController,
                 lockscreenWallpaperLazy,
                 biometricUnlockControllerLazy,
