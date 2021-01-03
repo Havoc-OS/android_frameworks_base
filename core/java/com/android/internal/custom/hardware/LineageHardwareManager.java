6bd631aa4b11edbe7fff34a11fb2f4cf02911b93
Arian <arian.kulmer@web.de>
2021-01-03 16:49:50 +0100

    LineageHardwareManager: Introduce keys swap feature support
    @jhenrique09 edits: Adapt to AOSP
    
    Change-Id: I789814d780b06b146c1f9ab62293f4d6ca287208
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/com/android/internal/custom/hardware/LineageHardwareManager.java
+++ core/java/com/android/internal/custom/hardware/LineageHardwareManager.java
@@ -44,6 +44,7 @@ import vendor.lineage.livedisplay.V2_0.IReadingEnhancement;
 import vendor.lineage.livedisplay.V2_0.ISunlightEnhancement;
 import vendor.lineage.touch.V1_0.IGloveMode;
 import vendor.lineage.touch.V1_0.IKeyDisabler;
+import vendor.lineage.touch.V1_0.IKeySwapper;
 import vendor.lineage.touch.V1_0.IStylusMode;
 import vendor.lineage.touch.V1_0.ITouchscreenGesture;
 
@@ -104,6 +105,12 @@ public final class LineageHardwareManager {
     public static final int FEATURE_KEY_DISABLE = 0x20;
 
     /**
+     * Hardware navigation key swapping
+     */
+    @VisibleForTesting
+    public static final int FEATURE_KEY_SWAP = 0x40;
+
+    /**
      * Increased display readability in bright light
      */
     @VisibleForTesting
@@ -157,6 +164,7 @@ public final class LineageHardwareManager {
         FEATURE_COLOR_ENHANCEMENT,
         FEATURE_HIGH_TOUCH_SENSITIVITY,
         FEATURE_KEY_DISABLE,
+        FEATURE_KEY_SWAP,
         FEATURE_SUNLIGHT_ENHANCEMENT,
         FEATURE_TOUCH_HOVERING,
         FEATURE_READING_ENHANCEMENT
@@ -283,6 +291,8 @@ public final class LineageHardwareManager {
                     return IGloveMode.getService(true);
                 case FEATURE_KEY_DISABLE:
                     return IKeyDisabler.getService(true);
+                case FEATURE_KEY_SWAP:
+                    return IKeySwapper.getService(true);
                 case FEATURE_TOUCH_HOVERING:
                     return IStylusMode.getService(true);
                 case FEATURE_TOUCHSCREEN_GESTURES:
@@ -346,6 +356,9 @@ public final class LineageHardwareManager {
                     case FEATURE_KEY_DISABLE:
                         IKeyDisabler keyDisabler = (IKeyDisabler) obj;
                         return keyDisabler.isEnabled();
+                    case FEATURE_KEY_SWAP:
+                        IKeySwapper keySwapper = (IKeySwapper) obj;
+                        return keySwapper.isEnabled();
                     case FEATURE_SUNLIGHT_ENHANCEMENT:
                         ISunlightEnhancement sunlightEnhancement = (ISunlightEnhancement) obj;
                         return sunlightEnhancement.isEnabled();
@@ -398,6 +411,9 @@ public final class LineageHardwareManager {
                     case FEATURE_KEY_DISABLE:
                         IKeyDisabler keyDisabler = (IKeyDisabler) obj;
                         return keyDisabler.setEnabled(enable);
+                    case FEATURE_KEY_SWAP:
+                        IKeySwapper keySwapper = (IKeySwapper) obj;
+                        return keySwapper.setEnabled(enable);
                     case FEATURE_SUNLIGHT_ENHANCEMENT:
                         ISunlightEnhancement sunlightEnhancement = (ISunlightEnhancement) obj;
                         return sunlightEnhancement.setEnabled(enable);
