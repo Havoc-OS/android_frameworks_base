f8a44220cb63e38545d9dc162c0597abaf20b612
Anushek Prasal <anushekprasal@gmail.com>
2021-01-30 14:21:28 +0530

    GamingMode: Re-enable HW keys toggle [1/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Iad18f9c39679fcefa54d99a24414027677e9cd9f
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/com/android/internal/util/custom/GamingModeController.java
+++ core/java/com/android/internal/util/custom/GamingModeController.java
@@ -253,10 +253,10 @@ public class GamingModeController {
         boolean disableHwKeys = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.GAMING_MODE_HW_KEYS_TOGGLE, 0) == 1;
         if (disableHwKeys) {
-            // mHwKeysState = Settings.Secure.getInt(mContext.getContentResolver(),
-            //                  Settings.Secure.HARDWARE_KEYS_DISABLE, 0);
-            // Settings.Secure.putInt(mContext.getContentResolver(),
-            //    Settings.Secure.HARDWARE_KEYS_DISABLE, 1);
+            mHwKeysState = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_ENABLE, 1);
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_ENABLE, 0);
         }
 
         // Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
@@ -298,8 +298,8 @@ public class GamingModeController {
         boolean disableHwKeys = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.GAMING_MODE_HW_KEYS_TOGGLE, 0) == 1;
         if (disableHwKeys) {
-            // Settings.Secure.putInt(mContext.getContentResolver(),
-            //    Settings.Secure.HARDWARE_KEYS_DISABLE, mHwKeysState);
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HARDWARE_KEYS_ENABLE, mHwKeysState);
         }
         int ringerMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.GAMING_MODE_RINGER_MODE, 0);
