37d7412da41a49353ed146ed836fc41e5a05ae05
Pranav Vashi <neobuddy89@gmail.com>
2021-02-02 02:28:47 +0530

    QuickStatusBar: Check for privacy chip indicators earlier
    This fixes bug where indicator is visible in both quick status bar and QS panel
    We had similar fix on 10.0: https://github.com/crdroidandroid/android_frameworks_base/commit/145ed309bd032d2fbbebd2f42a2a4a3cf950bfdd
    
    Issue cause:
     getIgnoredIconSlots() checks for getChipEnabled()
     getChipEnabled() checks for mMicCameraIndicatorsEnabled || mAllIndicatorsEnabled
     Therefore set mMicCameraIndicatorsEnabled, mAllIndicatorsEnabled before getIgnoredIconSlots()
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Ia7c742a2d96283f4230484a37256557268548c1d
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -247,6 +247,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mSystemIconsView = findViewById(R.id.quick_status_bar_system_icons);
         mQuickQsStatusIcons = findViewById(R.id.quick_qs_status_icons);
         StatusIconContainer iconContainer = findViewById(R.id.statusIcons);
+
+        mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
+        mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
+
         // Ignore privacy icons because they show in the space above QQS
         iconContainer.addIgnoredSlots(getIgnoredIconSlots());
         iconContainer.setShouldRestrictIcons(false);
@@ -311,9 +315,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mNextAlarmTextView.setSelected(true);
 
         mBatteryInQS = getResources().getBoolean(R.bool.config_batteryInQSPanel);
-
-        mAllIndicatorsEnabled = mPrivacyItemController.getAllIndicatorsAvailable();
-        mMicCameraIndicatorsEnabled = mPrivacyItemController.getMicCameraAvailable();
     }
 
     public QuickQSPanel getHeaderQsPanel() {
