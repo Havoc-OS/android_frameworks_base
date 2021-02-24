8dc0485acb0b6979223ea2abbb116ef8e54c4a2d
daniml3 <danimoral1001@gmail.com>
2021-02-24 22:09:30 +0100

    SystemUI: Fix possible incorrect position of QS page indicator
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I88bc3279552beaf4b68f4ea5d2ee2b9fd52eb0fc
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -315,6 +315,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         post(() -> {
             updateVisibilities();
             updateClickabilities();
+            mQsPanel.setFooterPageIndicator(mPageIndicator);
             setClickable(false);
         });
     }
