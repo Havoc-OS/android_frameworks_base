1335e9084cc3b26785ed7b594b85cebfedb316d9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-13 14:57:01 +0530

    base: Make power button press fp unlock toggleable [1/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -697,9 +697,7 @@ public abstract class BiometricServiceBase extends SystemService
     @Override
     public void onStart() {
         listenForUserSwitches();
-        if (Utils.hasPowerButtonFingerprint(mContext)) {
-            registerForWindowManger();
-        }
+        registerForWindowManger();
     }
 
     @Override
