a9957e8881564d8286bfc838857bed9c40fa892a
LuK1337 <priv.luk@gmail.com>
2021-04-24 18:14:46 +0200

    Keyguard: Fix mapping mismatch between KeyEvent and NumPadKey
    Change-Id: Ida28a4f037c2f3fa843ac136779e026020d711db
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -56,6 +56,7 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private View mDivider;
     private int mDisappearYTranslation;
     private View[][] mViews;
+    private boolean mScramblePin;
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
 
     private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
@@ -137,10 +138,9 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
             });
         }
 
-        boolean scramblePin = (Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0, UserHandle.USER_CURRENT) == 1);
-
-        if (scramblePin) {
+        mScramblePin = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0, UserHandle.USER_CURRENT) == 1;
+        if (mScramblePin) {
             Collections.shuffle(sNumbers);
             // get all children who are NumPadKey's
             LinearLayout container = (LinearLayout) findViewById(R.id.container);
@@ -235,6 +235,14 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         mRow2.setClipToPadding(enable);
         mRow3.setClipToPadding(enable);
         setClipChildren(enable);
+    }
+
+    @Override
+    protected int getNumberIndex(int number) {
+        if (mScramblePin) {
+            return (sNumbers.indexOf(number) + 1) % sNumbers.size();
+        }
+        return super.getNumberIndex(number);
     }
 
     @Override
