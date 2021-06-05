7ff0e8b381ba329211a8d06d438cce71874ad3a0
Ido Ben-Hur <idoybh2@gmail.com>
2021-06-05 12:05:58 +0300

    VolumeDialogImpl: Fix collapse animation when on right
    The rows should be gone before the animation starts in this case
    
    Change-Id: I01c270fdb31cdd2f920fe29626b6a76c02882f58
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -645,6 +645,7 @@ public class VolumeDialogImpl implements VolumeDialog,
             mDialogRowsView.measure(WRAP_CONTENT, WRAP_CONTENT);
             targetWidth = mDialogRowsView.getMeasuredWidth();
         } else {
+            if (!mVolumePanelOnLeft) updateExpandedRows(false);
             targetWidth = mContext.getResources().getDimensionPixelSize(
                     R.dimen.volume_dialog_panel_width);
         }
