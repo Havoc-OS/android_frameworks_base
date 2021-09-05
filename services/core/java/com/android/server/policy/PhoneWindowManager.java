4976799b9f68e969b0f71dd3b6e9b004d27bd514
LuK1337 <priv.luk@gmail.com>
2021-09-05 23:28:25 +0200

    Disallow click to partial screenshot right after screenshot is taken
    This change prevents triggering partial screenshot when trying to take
    full screenshot of protected window.
    
    Change-Id: I42a67ed1ee34edffccc6dacd17ba1dffc8d194b3
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -537,6 +537,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // Click volume down + power for partial screenshot
     boolean mClickPartialScreenshot;
+    boolean mClickPartialScreenshotAllowed = true;
 
     private boolean mPendingKeyguardOccluded;
     private boolean mKeyguardOccludedChanged;
@@ -1714,7 +1715,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             if (!mPocketLockShowing) {
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, mScreenshotSource);
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, mScreenshotSource,
+                        uri -> { mClickPartialScreenshotAllowed = false; });
             }
         }
     }
@@ -4540,10 +4542,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         cancelPendingScreenshotChordAction();
                         cancelPendingAccessibilityShortcutAction();
 
-                        if (mClickPartialScreenshot && mScreenshotChordVolumeDownKeyConsumed) {
+                        if (mClickPartialScreenshot && mClickPartialScreenshotAllowed &&
+                                mScreenshotChordVolumeDownKeyConsumed) {
                             mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                             mHandler.post(mScreenshotRunnable);
                         }
+
+                        mClickPartialScreenshotAllowed = true;
                     }
                 } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
                     if (down) {
