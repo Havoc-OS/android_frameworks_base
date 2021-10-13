2eb6797738d55e7bc2ab9863176ffd16c6bdffdc
Matt Mower <mowerm@gmail.com>
2021-10-13 09:22:28 +0000

    SystemUI: Implement lockscreen quick unlock
    [BigBrother1984 <carlosavignano@aospa.co>]:
        * PasswordTextView QuickUnlock listener functions and method
    
    [mydongistiny <jaysonedson@gmail.com>]:
        * Forward port to Android 8 / 10
    
    [SagarMakhar <sagarmakhar@gmail.com>]:
        * Forward port to Android 12
    
    [Hikari-no-Tenshi <kyryljan.serhij@gmail.com>]:
        * Make Quick Unlock compatible with long PIN/Password (Idea from OOS)
        * Use AsyncTask for PIN/Password check.
        * Restore call on main thread check.
    
    [Hernán Castañón <herna@paranoidandroid.co>]:
        * Make it independent of toggle (enabled by default)
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
        * Code cleanup and format
        * Hide Ok button in favour of quick unlock
        * Move backspace button to right
    
    [ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>]:
        * Simplify and refactor to use verifyPasswordAndUnlock()
        * Handle case where password length is not set
        * Hide backspace button when PIN is empty
        * Adapt to 12L - thanks to Jyotiraditya
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hernán Castañón <herna@paranoidandroid.co>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: BigBrother1984 <carlosavignano@aospa.co>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I1d721ad454fe4a37868071ef978468a12c844a03
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardAbsKeyInputViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardAbsKeyInputViewController.java
@@ -171,6 +171,9 @@ public abstract class KeyguardAbsKeyInputViewController<T extends KeyguardAbsKey
     void onPasswordChecked(int userId, boolean matched, int timeoutMs, boolean isValidPassword) {
         boolean dismissKeyguard = KeyguardUpdateMonitor.getCurrentUser() == userId;
         if (matched) {
+            if (mLockPatternUtils.getPinPasswordLength(userId) == -1) {
+                mLockPatternUtils.setPinPasswordLength(mView.getEnteredCredential().size(), userId);
+            }
             getKeyguardSecurityCallback().reportUnlockAttempt(userId, true, 0);
             if (dismissKeyguard) {
                 mDismissing = true;
