07e71118b721a77eb9e60e8d96515deeb5a63a94
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-11 16:59:36 +0530

    SystemUI: Refactor statusbar bluetooth icon
    Move it to its own layout like wifi and mobile,
    and separate the bluetooth connected and battery
    icons.
    
    This allows us to use bluetooth connected icon
    from icon packs, and also color the battery icon
    when its at low battery (in the next change).
    
    While we're at it, simplify the logic used to
    determine drawable from battery level, and also
    add icon for <10% battery.
    
    Change-Id: I5dd50d84497816484e1c33a7432a3d85c3a7f79d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/8626
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
@@ -23,6 +23,7 @@ import android.graphics.drawable.Icon;
 import android.os.UserHandle;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.BluetoothIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.MobileIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.WifiIconState;
@@ -63,6 +64,8 @@ public class StatusBarIconHolder {
     @Deprecated
     public static final int TYPE_WIFI_NEW = 4;
 
+    public static final int TYPE_BLUETOOTH = 5;
+
     @IntDef({
             TYPE_ICON,
             TYPE_WIFI,
@@ -76,6 +79,7 @@ public class StatusBarIconHolder {
     private StatusBarIcon mIcon;
     private WifiIconState mWifiState;
     private MobileIconState mMobileState;
+    private BluetoothIconState mBluetoothState;
     private @IconType int mType = TYPE_ICON;
     private int mTag = 0;
 
@@ -133,6 +137,14 @@ public class StatusBarIconHolder {
         holder.mMobileState = state;
         holder.mType = TYPE_MOBILE;
         holder.mTag = state.subId;
+        return holder;
+    }
+
+    /** */
+    public static StatusBarIconHolder fromBluetoothIconState(BluetoothIconState state) {
+        StatusBarIconHolder holder = new StatusBarIconHolder();
+        holder.mBluetoothState = state;
+        holder.mType = TYPE_BLUETOOTH;
         return holder;
     }
 
@@ -195,6 +207,15 @@ public class StatusBarIconHolder {
         mMobileState = state;
     }
 
+    @Nullable
+    public BluetoothIconState getBluetoothState() {
+        return mBluetoothState;
+    }
+
+    public void setBluetoothState(BluetoothIconState state) {
+        mBluetoothState = state;
+    }
+
     public boolean isVisible() {
         switch (mType) {
             case TYPE_ICON:
@@ -208,6 +229,8 @@ public class StatusBarIconHolder {
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // this is effectively an unused return value.
                 return true;
+            case TYPE_BLUETOOTH:
+		        return mBluetoothState.visible;
             default:
                 return true;
         }
@@ -235,6 +258,10 @@ public class StatusBarIconHolder {
             case TYPE_WIFI_NEW:
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // ignore setVisible.
+                break;
+
+            case TYPE_BLUETOOTH:
+                mBluetoothState.visible = visible;
                 break;
         }
     }
