6b5a9af4c3ebc9212f8709db33a75279cc93e01e
Pranav Vashi <neobuddy89@gmail.com>
2022-09-29 19:16:50 +0530

    Revert "SystemUI: Network Traffic [1/3]"
    This reverts commit 797c882b432243cdbf347f4a1811ed78c6eecaf4.
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -117,9 +117,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private LinearLayout mEndSideContent;
     private View mOngoingCallChip;
     private View mNotificationIconAreaInner;
-    private View mNetworkTrafficHolderStart;
-    private View mNetworkTrafficHolderCenter;
-    private View mNetworkTrafficHolderEnd;
     private int mDisabled1;
     private int mDisabled2;
     private DarkIconManager mDarkIconManager;
@@ -291,9 +288,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         updateBlockedIcons();
         mStatusBarIconController.addIconGroup(mDarkIconManager);
         mEndSideContent = mStatusBar.findViewById(R.id.status_bar_end_side_content);
-        mNetworkTrafficHolderStart = mStatusBar.findViewById(R.id.network_traffic_holder_start);
-        mNetworkTrafficHolderCenter = mStatusBar.findViewById(R.id.network_traffic_holder_center);
-        mNetworkTrafficHolderEnd = mStatusBar.findViewById(R.id.network_traffic_holder_end);
         mClockController = mStatusBar.getClockController();
         mOngoingCallChip = mStatusBar.findViewById(R.id.ongoing_call_chip);
         showEndSideContent(false);
@@ -573,9 +567,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private void hideEndSideContent(boolean animate) {
         animateHide(mEndSideContent, animate);
-        animateHide(mNetworkTrafficHolderStart, animate);
-        animateHide(mNetworkTrafficHolderCenter, animate);
-        animateHide(mNetworkTrafficHolderEnd, animate);
     }
 
     private void showEndSideContent(boolean animate) {
@@ -583,16 +574,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         int state = mAnimationScheduler.getAnimationState();
         if (state == IDLE || state == SHOWING_PERSISTENT_DOT) {
             animateShow(mEndSideContent, animate);
-            animateShow(mNetworkTrafficHolderStart, animate);
-            animateShow(mNetworkTrafficHolderCenter, animate);
-            animateShow(mNetworkTrafficHolderEnd, animate);
         } else {
             // We are in the middle of a system status event animation, which will animate the
             // alpha (but not the visibility). Allow the view to become visible again
             mEndSideContent.setVisibility(View.VISIBLE);
-            mNetworkTrafficHolderStart.setVisibility(View.VISIBLE);
-            mNetworkTrafficHolderCenter.setVisibility(View.VISIBLE);
-            mNetworkTrafficHolderEnd.setVisibility(View.VISIBLE);
         }
     }
 
