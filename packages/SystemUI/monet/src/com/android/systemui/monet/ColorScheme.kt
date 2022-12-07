cef5b1a9e863f873dfbf6ef780a7c5a4773c2132
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-07 15:22:05 +0200

    SystemUI: ColorScheme: Limit custom tinting to accent1
    We don't wanna control secondary, ternary colors
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/monet/src/com/android/systemui/monet/ColorScheme.kt
+++ packages/SystemUI/monet/src/com/android/systemui/monet/ColorScheme.kt
@@ -322,8 +322,8 @@ class ColorScheme(
         }
 
         accent1 = TonalPalette(style.coreSpec.a1, seedArgb, luminanceFactor, chromaFactor)
-        accent2 = TonalPalette(style.coreSpec.a2, seedArgb, luminanceFactor, chromaFactor)
-        accent3 = TonalPalette(style.coreSpec.a3, seedArgb, luminanceFactor, chromaFactor)
+        accent2 = TonalPalette(style.coreSpec.a2, seedArgb)
+        accent3 = TonalPalette(style.coreSpec.a3, seedArgb)
         neutral1 = TonalPalette(style.coreSpec.n1, seedArgb)
         neutral2 = TonalPalette(style.coreSpec.n2, seedArgb)
     }
