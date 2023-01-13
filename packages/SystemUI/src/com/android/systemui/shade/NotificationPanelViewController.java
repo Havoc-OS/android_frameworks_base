570f5497d4d9523b61a30219c41e0d8ac5af0d9d
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-13 14:22:37 +0200

    SystemUI: Don't force small clock when media isn't allowed on keyguard
    test: hide the media player from lockscreen via settings, have the double-line clock enabled,
    have all notifications dismissed - observe large clock still showing
    
    Change-Id: Ic6cfbb8e5f60b69401fc69ca44e2d879b506003d
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1587,7 +1587,8 @@ public final class NotificationPanelViewController implements Dumpable {
     private int computeDesiredClockSizeForSplitShade() {
         // Media is not visible to the user on AOD.
         boolean isMediaVisibleToUser =
-                mMediaDataManager.hasActiveMediaOrRecommendation() && !isOnAod();
+                mMediaDataManager.hasActiveMediaOrRecommendation() && !isOnAod()
+                && mMediaHierarchyManager.getShouldShowOnLockScreen();
         if (isMediaVisibleToUser) {
             // When media is visible, it overlaps with the large clock. Use small clock instead.
             return SMALL;
@@ -1705,9 +1706,10 @@ public final class NotificationPanelViewController implements Dumpable {
     }
 
     private boolean hasVisibleNotifications() {
+        final boolean mediaVisible = mMediaDataManager.hasActiveMediaOrRecommendation()
+                && mMediaHierarchyManager.getShouldShowOnLockScreen();
         return mNotificationStackScrollLayoutController
-                .getVisibleNotificationCount() != 0
-                || mMediaDataManager.hasActiveMediaOrRecommendation();
+                .getVisibleNotificationCount() != 0 || mediaVisible;
     }
 
     /** Returns space between top of lock icon and bottom of NotificationStackScrollLayout. */
