82018ace4f3998d61e32b8f37dc90194bb49795c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-10 20:18:18 +0800

    ConfigurationController: Prevent app crash caused by null configuration
    fixes:
    * https://bin.kv2.dev/~640b1b26e599576ea02bdd81
    
    test: open app in bug, no crashes after patch
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/app/ConfigurationController.java
+++ core/java/android/app/ConfigurationController.java
@@ -216,7 +216,7 @@ class ConfigurationController {
             final int size = callbacks.size();
             for (int i = 0; i < size; i++) {
                 ComponentCallbacks2 cb = callbacks.get(i);
-                if (!equivalent) {
+                if (!equivalent && cb != null && config != null) {
                     performConfigurationChanged(cb, config);
                 }
             }
@@ -267,6 +267,7 @@ class ConfigurationController {
      * original LocaleList.
      */
     void updateLocaleListFromAppContext(@NonNull Context context) {
+        if (context.getResources().getConfiguration() == null || mResourcesManager.getConfiguration() == null || mResourcesManager == null || context.getResources() == null) return;
         final Locale bestLocale = context.getResources().getConfiguration().getLocales().get(0);
         final LocaleList newLocaleList = mResourcesManager.getConfiguration().getLocales();
         final int newLocaleListSize = newLocaleList.size();
