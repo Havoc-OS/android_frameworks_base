48b37474aba6c86ada509dfd2946b8112c74c596
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-20 07:03:18 +0000

    base: Hide IME space feature [1/2] * hide rotating ime back button
    * Disable it by default
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I6f1fef4c3795e83934cd51936fb7129bfafd3a11
    
    -----
    
    base: Make NavigationBar to use Hidden IME Space only on gesture bar
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I90afdf5362e77574b464d78a2189fb5e521a5837
    Co-authored-by: KrutosVIP <krutosviprus@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5546,6 +5546,12 @@ public final class Settings {
         public static final String DESKTOP_MODE = "desktop_mode";
 
         /**
+         * Whether to show IME space
+         * @hide
+         */
+        public static final String HIDE_IME_SPACE_ENABLE = "hide_ime_space_enable";
+
+        /**
          * Volume keys control cursor in text fields (default is 0)
          * 0 - Disabled
          * 1 - Volume up/down moves cursor left/right
