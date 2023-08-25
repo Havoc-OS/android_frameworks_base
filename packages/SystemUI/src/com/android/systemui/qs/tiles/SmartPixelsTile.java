jimgsey/android_frameworks_base-1

[SQUASH] SystemUI: Smart Pixels [1/2]
SystemUI: screen-dimmer-pixel-filter

Major credits to Sergii Pylypenko

Change-Id: Ib2d7e18ad8fe2313dbf7593bf55a2cfec03ce567
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

SystemUI: Smart Pixels [1/2]

Disables a percentage of pixels on screen to reduce power consumption.
If enabled with battery saver, don't scale brightness at 0.5f for UX.

Includes:
 - Option to enable on battery saver
 - User chosen grid
 - Burn-in protection

Configurable via overlay and disabled by defualt:
  "config_supportSmartPixels"

Change-Id: Id3c78548cb090ab2da11f543da31c5a408fb9fe9
Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

Smart Pixels: Switch to registered receiver

Switching to a registered receiver allows to properly handle updates
on enabling of battery saver mode and switching of users.

Also only update screen filter with burn-in protection when the
device is in an interactive state.

Test: Service starts after rebooting with it enabled
      Service starts on battery saver mode (user toggle)
      Service starts on battery saver mode (auto-enabled)
      Service re-adjusts on user switch to current user settings
      Filter updates after selected timeout

Change-Id: Iced17fd5cc49e0163754bf75782f8465b54e859b
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

Smart Pixels: Dynamically register receiver

Don't keep the receiver registered if it isn't enabled.

Change-Id: If6975df536598ee19d0ee17ec4150ae1b055e18c
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

SmartPixels: Use CoreStartable interface for receiver

* Also clean up and add check whether smart pixels is supported.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

SystemUI: Add Smart Pixels tile

Single tap enables/disables.
Long press opens Smart Pixels settings.

User is not allowed to enable or disable when
Smart Pixels has been auto-ena

packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -1,0 +1,172 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ * Copyright (C) 2018 Adin Kwok (adinkwok)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.PowerManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.BatteryController;
+
+import javax.inject.Inject;
+
+public class SmartPixelsTile extends QSTileImpl<BooleanState> implements
+        BatteryController.BatteryStateChangeCallback {
+
+    private static final Intent SMART_PIXELS_SETTINGS = new Intent("android.settings.SMART_PIXELS_SETTINGS");
+
+    private final BatteryController mBatteryController;
+
+    private boolean mSmartPixelsEnable;
+    private boolean mSmartPixelsOnPowerSave;
+    private boolean mLowPowerMode;
+    private boolean mListening;
+
+    @Inject
+    public SmartPixelsTile(QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mBatteryController = Dependency.get(BatteryController.class);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (listening) {
+            mBatteryController.addCallback(this);
+        } else {
+            mBatteryController.removeCallback(this);
+        }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_supportSmartPixels);
+    }
+
+    @Override
+    public void handleClick(View v) {
+        mSmartPixelsEnable = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT) == 1);
+        mSmartPixelsOnPowerSave = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        if (mLowPowerMode && mSmartPixelsOnPowerSave) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                    0, UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    0, UserHandle.USER_CURRENT);
+        } else if (!mSmartPixelsEnable) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    1, UserHandle.USER_CURRENT);
+        } else {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    0, UserHandle.USER_CURRENT);
+        }
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return SMART_PIXELS_SETTINGS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        mSmartPixelsEnable = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT) == 1);
+        mSmartPixelsOnPowerSave = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        state.icon  = ResourceIcon.get(R.drawable.ic_qs_smart_pixels);
+        if (state.slash == null) {
+            state.slash = new SlashState();
+        }
+        if (mLowPowerMode && mSmartPixelsOnPowerSave) {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels_on_power_save);
+            state.value = true;
+        } else if (mSmartPixelsEnable) {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels);
+            state.value = true;
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels);
+            state.value = false;
+        }
+        state.slash.isSlashed = !state.value;
+        state.state = state.value ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_smart_pixels);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    @Override
+    public void onBatteryLevelChanged(int level, boolean plugged, boolean charging) {
+        // yurt
+    }
+
+    @Override
+    public void onPowerSaveChanged(boolean active) {
+        mLowPowerMode = active;
+        refreshState();
+    }
+}

Havoc-OS/android_frameworks_base

Change-Id: Idf80bd3b54ddfde50ed0993c99076cbdf6b838f1
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -1,0 +1,176 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ * Copyright (C) 2018 Adin Kwok (adinkwok)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.PowerManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.BatteryController;
+
+import javax.inject.Inject;
+
+public class SmartPixelsTile extends QSTileImpl<BooleanState> implements
+        BatteryController.BatteryStateChangeCallback {
+
+    public static final String TILE_SPEC = "smartpixels";
+
+
+    private static final Intent SMART_PIXELS_SETTINGS = new Intent("android.settings.SMART_PIXELS_SETTINGS");
+
+    private final BatteryController mBatteryController;
+
+    private boolean mSmartPixelsEnable;
+    private boolean mSmartPixelsOnPowerSave;
+    private boolean mLowPowerMode;
+    private boolean mListening;
+
+    @Inject
+    public SmartPixelsTile(QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger,
+            BatteryController batteryController) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mBatteryController = batteryController;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (listening) {
+            mBatteryController.addCallback(this);
+        } else {
+            mBatteryController.removeCallback(this);
+        }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_supportSmartPixels);
+    }
+
+    @Override
+    public void handleClick(View v) {
+        mSmartPixelsEnable = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT) == 1);
+        mSmartPixelsOnPowerSave = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        if (mLowPowerMode && mSmartPixelsOnPowerSave) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                    0, UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    0, UserHandle.USER_CURRENT);
+        } else if (!mSmartPixelsEnable) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    1, UserHandle.USER_CURRENT);
+        } else {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.SMART_PIXELS_ENABLE,
+                    0, UserHandle.USER_CURRENT);
+        }
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return SMART_PIXELS_SETTINGS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        mSmartPixelsEnable = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT) == 1);
+        mSmartPixelsOnPowerSave = (Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT) == 1);
+        state.icon  = ResourceIcon.get(R.drawable.ic_qs_smart_pixels);
+        if (state.slash == null) {
+            state.slash = new SlashState();
+        }
+        if (mLowPowerMode && mSmartPixelsOnPowerSave) {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels_on_power_save);
+            state.value = true;
+        } else if (mSmartPixelsEnable) {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels);
+            state.value = true;
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_smart_pixels);
+            state.value = false;
+        }
+        state.slash.isSlashed = !state.value;
+        state.state = state.value ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_smart_pixels);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    @Override
+    public void onBatteryLevelChanged(int level, boolean plugged, boolean charging) {
+        // yurt
+    }
+
+    @Override
+    public void onPowerSaveChanged(boolean active) {
+        mLowPowerMode = active;
+        refreshState();
+    }
+}
+